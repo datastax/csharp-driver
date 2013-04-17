691fb70cf92a1fba4fc1a4cc29c4f589445884e8
Pawel Kaplanski <p.kaplanski@cognitum.eu>
2013-04-17 19:56:29 +0200

    CCMBridgeTester

---
+++ CCMBridgeTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CCMBridgeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cluster = Cassandra.CCMBridge.Create("test", 3))
+            {
+                cluster.stop();
+                cluster.start();
+            }
+        }
+    }
+}
