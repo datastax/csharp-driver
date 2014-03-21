bf241c8f83b6314e4c73f87345576bd908464e7a
Pawel Kaplanski <p.kaplanski@cognitum.eu>
2014-03-22 00:55:32 +0100

    release 2.0.1

--- make.bat
+++ make.bat
@@ -3,14 +3,15 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 msbuild Cassandra.MyTest.sln /t:Clean /p:Configuration=Release
 msbuild Cassandra.MyTest.sln /p:Configuration=Release
 mkdir "Nuget\lib"
-cp Cassandra/bin/Release/Cassandra.dll Nuget/lib/Cassandra.dll
-cp Cassandra.Data/bin/Release/Cassandra.Data.dll Nuget/lib/Cassandra.Data.dll
-cp Cassandra.Data.Linq/bin/Release/Cassandra.Data.Linq.dll Nuget/lib/Cassandra.Data.Linq.dll
-cp Cassandra.Data.DSE/bin/Release/Cassandra.Data.DSE.dll Nuget/lib/Cassandra.Data.DSE.dll
-cp Cassandra/bin/Release/Cassandra.xml Nuget/lib/Cassandra.xml
-cp Cassandra.Data/bin/Release/Cassandra.Data.xml Nuget/lib/Cassandra.Data.xml
-cp Cassandra.Data.Linq/bin/Release/Cassandra.Data.Linq.xml Nuget/lib/Cassandra.Data.Linq.xml
-cp Cassandra.Data.DSE/bin/Release/Cassandra.Data.DSE.xml Nuget/lib/Cassandra.Data.DSE.xml
+copy Cassandra\bin\Release\Cassandra.dll Nuget\lib\Cassandra.dll
+copy Cassandra.Data\bin\Release\Cassandra.Data.dll Nuget\lib\Cassandra.Data.dll
+copy Cassandra.Data.Linq\bin\Release\Cassandra.Data.Linq.dll Nuget\lib\Cassandra.Data.Linq.dll
+copy Cassandra.DSE\bin\Release\Cassandra.DSE.dll Nuget\lib\Cassandra.DSE.dll
+
+copy Cassandra\bin\Release\Cassandra.xml Nuget\lib\Cassandra.xml
+copy Cassandra.Data\bin\Release\Cassandra.Data.xml Nuget\lib\Cassandra.Data.xml
+copy Cassandra.Data.Linq\bin\Release\Cassandra.Data.Linq.xml Nuget\lib\Cassandra.Data.Linq.xml
+copy Cassandra.DSE\bin\Release\Cassandra.DSE.xml Nuget\lib\Cassandra.DSE.xml
 "tools\nuget" pack "nuget\CassandraCSharpDriver.nuspec"
-rmdir /S /Q "Nuget\lib"
-msbuild Documentation.sln /p:Configuration=Release
+rem rmdir \S \Q "Nuget\lib"
+rem msbuild Documentation.sln \p:Configuration=Release
