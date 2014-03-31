89feb4e40c43dde1c7f74267afa83bc9f3f9d9fa
Luke Tillman <luke.tillman@gmail.com>
2014-03-31 10:07:06 -0400

    Move all integration tests (which is currently all tests) into a new, single library (Cassandra.IntegrationTests)
    - This includes CCMBridge and MyTestLib
    - Namespaces are all currently wrong, but I'll fix that in a bit
    - Also rename the test runner from MyTestRun to Cassandra.IntegrationTests.Runner
    - Things still compile after this move, but the tests are probably broken due to their usage of reflection
    
    Conflicts:
    	src/Cassandra.Data.Linq.MSTest/Cassandra.Data.Linq.MyTest.csproj
    	src/packages/repositories.config

--- src/Cassandra.IntegrationTests/Properties/AssemblyInfo.cs
+++ src/Cassandra.IntegrationTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cassandra.Data.MSTest")]
+[assembly: AssemblyTitle("Cassandra.IntegrationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cassandra.Data.MSTest")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Cassandra.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5d7c081c-c08a-4464-99bf-606cf83d39f5")]
+[assembly: Guid("9a04bee4-9614-42ed-a04e-3f0b65849a02")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
