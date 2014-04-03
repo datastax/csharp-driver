b0491d43083f1dee391028b53bafa97dd19f78f4
vytautassurvila <vytautas.survila@gmail.com>
2014-04-03 17:14:12 +0300

    USING TIMESTAMP in microseconds

--- Cassandra.Data.Linq/CqlQueryTools.cs
+++ Cassandra.Data.Linq/CqlQueryTools.cs
@@ -470,7 +470,7 @@ namespace Cassandra.Data.Linq
             if (timestamp != null)
             {
                 sb.Append("TIMESTAMP ");
-                sb.Append(Convert.ToInt64(Math.Floor((timestamp.Value - CqlQueryTools.UnixStart).TotalMilliseconds)));
+                sb.Append((timestamp.Value - CqlQueryTools.UnixStart).Ticks / 10);
             }
             return sb.ToString();
         }
