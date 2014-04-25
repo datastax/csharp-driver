51cf7ada6883e834fdaed8d47e9c801c9d924e6e
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-04-25 14:49:44 +0200

    Created a different solution file for the samples.
    Removed legacy test runner.

--- src/Cassandra.IntegrationTests.Runner/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// See the SharedAssemblyInfo.cs file for information shared by all assemblies
-[assembly: AssemblyTitle("Cassandra.IntegrationTests.Runner")]
-[assembly: AssemblyDescription("A custom test runner for integration tests")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2f3ab06e-8eb3-4c97-9df8-5ed2ac706190")]
--- src/Cassandra.IntegrationTests.Runner/README.rst
+++
@@ -1,3 +0,0 @@
-TestRunner Unit test environment
-===========
-
