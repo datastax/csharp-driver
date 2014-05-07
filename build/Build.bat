817ea667e9c3a24a56c3f41ad9eaae50649d0cdd
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-05-07 15:45:25 +0200

    Added build script to run unit tests

--- build/Build.bat
+++ build/Build.bat
@@ -1,5 +1,8 @@
 @echo off
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /v:m %*
+
+set params="%1"
+if %params% == "" set params=/t:unit-test
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /v:m %params%
 
 REM package
 REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /v:m /t:package /p:BUILD_NUMBER=YYY /p:PACKAGE_VERSION=2.0.0-beta2
