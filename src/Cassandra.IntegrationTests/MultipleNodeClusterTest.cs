b04d8ddfdfd9314dfc1e128e898026f4036c55d6
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-05-09 13:13:43 +0200

    MultipleNodeClusterTest base class

---
+++ src/Cassandra.IntegrationTests/MultipleNodeClusterTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Cassandra.IntegrationTests
+{
+    /// <summary>
+    /// Represents a set of tests that reuse an test cluster of n node
+    /// </summary>
+    public class MultipleNodesClusterTest
+    {
+        protected virtual string CcmLocalConfigDir { get; set; }
+
+        protected virtual ISession Session { get; set; }
+
+        protected virtual int NodeLength { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of MultipleNodeCluster Test
+        /// </summary>
+        /// <param name="nodeLength">Determines the amount of nodes in the test cluster</param>
+        public MultipleNodesClusterTest(int nodeLength)
+        {
+            this.NodeLength = nodeLength;
+        }
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+            var keyspaceName = "tester";
+            if (ConfigurationManager.AppSettings["UseRemote"] == "true")
+            {
+                CCMBridge.ReusableCCMCluster.Setup(NodeLength);
+                CCMBridge.ReusableCCMCluster.Build(Cluster.Builder());
+                Session = CCMBridge.ReusableCCMCluster.Connect(keyspaceName);
+            }
+            else
+            {
+                //Create a local instance
+                CcmLocalConfigDir = TestUtils.CreateTempDirectory();
+                var output = TestUtils.ExecuteLocalCcmClusterStart(CcmLocalConfigDir, "2.0.6");
+
+                if (output.ExitCode != 0)
+                {
+                    throw new TestInfrastructureException("Local ccm could not start: " + output.ToString());
+                }
+                var cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+                Session = cluster.Connect();
+                Session.CreateKeyspaceIfNotExists(keyspaceName);
+                Session.ChangeKeyspace(keyspaceName);
+            }
+        }
+
+        [TearDown]
+        public virtual void Teardown()
+        {
+            if (ConfigurationManager.AppSettings["UseRemote"] == "true")
+            {
+                CCMBridge.ReusableCCMCluster.Drop();
+            }
+            else
+            {
+                try
+                {
+                    //Try to close the connections
+                    Session.Dispose();
+                }
+                catch
+                {
+
+                }
+                //Remove the cluster
+                TestUtils.ExecuteLocalCcmClusterRemove(CcmLocalConfigDir);
+            }
+        }
+    }
+}
--- src/Cassandra.IntegrationTests/SingleNodeClusterTest.cs
+++ src/Cassandra.IntegrationTests/SingleNodeClusterTest.cs
@@ -9,69 +9,16 @@ using System.Threading;
 
 namespace Cassandra.IntegrationTests
 {
+    /// <summary>
+    /// Represents a set of tests that reuse an test cluster of 1 node
+    /// </summary>
     [TestFixture]
-    public class SingleNodeClusterTest
+    public class SingleNodeClusterTest: MultipleNodesClusterTest
     {
-        public virtual string CcmLocalConfigDir { get; set; }
-
-        public virtual ISession Session { get; set; }
-
-        [SetUp]
-        public virtual void Setup()
+        public SingleNodeClusterTest() 
+            : base(1)
         {
-            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
-            var keyspaceName = "tester";
-            if (ConfigurationManager.AppSettings["UseRemote"] == "true")
-            {
-                CCMBridge.ReusableCCMCluster.Setup(1);
-                CCMBridge.ReusableCCMCluster.Build(Cluster.Builder());
-                Session = CCMBridge.ReusableCCMCluster.Connect(keyspaceName);
-            }
-            else
-            {
-                //Create a local instance
-                CcmLocalConfigDir = TestUtils.CreateTempDirectory();
-                var output = TestUtils.ExecuteLocalCcmClusterStart(CcmLocalConfigDir, "2.0.6");
 
-                if (output.ExitCode != 0)
-                {
-                    throw new TestInfrastructureException("Local ccm could not start: " + output.ToString());
-                }
-                var cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
-                Session = cluster.Connect();
-                Session.CreateKeyspaceIfNotExists(keyspaceName);
-                Session.ChangeKeyspace(keyspaceName);
-            }
-        }
-
-        [Test]
-        public void TestConnections()
-        {
-            var rs = Session.Execute("SELECT * FROM system.schema_keyspaces");
-            Assert.True(rs.Count() > 0);
-        }
-
-        [TearDown]
-        public virtual void Teardown()
-        {
-            if (ConfigurationManager.AppSettings["UseRemote"] == "true")
-            {
-                CCMBridge.ReusableCCMCluster.Drop();
-            }
-            else
-            {
-                try
-                {
-                    //Try to close the connections
-                    Session.Dispose();
-                }
-                catch
-                {
-
-                }
-                //Remove the cluster
-                TestUtils.ExecuteLocalCcmClusterRemove(CcmLocalConfigDir);
-            }
         }
     }
 }
