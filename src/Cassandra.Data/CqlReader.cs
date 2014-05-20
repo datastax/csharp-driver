77c937a6ea29a635632ff38be1e7a18145cd8006
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-05-20 11:10:00 +0200

    Doc comments: improved ADO.NET doc comments

--- src/Cassandra.Data/CqlReader.cs
+++ src/Cassandra.Data/CqlReader.cs
@@ -22,6 +22,10 @@ using System.Data.Common;
 
 namespace Cassandra.Data
 {
+    /// <summary>
+    /// Reads a forward-only stream of rows from Cassandra.
+    /// </summary>
+    /// <inheritdoc />
     public class CqlReader : DbDataReader
     {
         private readonly Dictionary<string, int> colidx = new Dictionary<string, int>();
@@ -34,6 +38,7 @@ namespace Cassandra.Data
             get { return 0; }
         }
 
+        /// <inheritdoc />
         public override int FieldCount
         {
             get { return popul.Columns.Length; }
@@ -78,46 +83,55 @@ namespace Cassandra.Data
 
         }
 
+        /// <inheritdoc />
         public override bool GetBoolean(int ordinal)
         {
             return (bool) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override byte GetByte(int ordinal)
         {
             return (byte) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override long GetBytes(int ordinal, long dataOffset, byte[] buffer, int bufferOffset, int length)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public override char GetChar(int ordinal)
         {
             return (char) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override long GetChars(int ordinal, long dataOffset, char[] buffer, int bufferOffset, int length)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public override string GetDataTypeName(int ordinal)
         {
             return popul.Columns[ordinal].TypeCode.ToString();
         }
 
+        /// <inheritdoc />
         public override DateTime GetDateTime(int ordinal)
         {
             return (DateTime) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override decimal GetDecimal(int ordinal)
         {
             return (decimal) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override double GetDouble(int ordinal)
         {
             return (double) GetValue(ordinal);
@@ -128,41 +142,49 @@ namespace Cassandra.Data
             return new DbEnumerator(this);
         }
 
+        /// <inheritdoc />
         public override Type GetFieldType(int ordinal)
         {
             return popul.Columns[ordinal].Type;
         }
 
+        /// <inheritdoc />
         public override float GetFloat(int ordinal)
         {
             return (float) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override Guid GetGuid(int ordinal)
         {
             return (Guid) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override short GetInt16(int ordinal)
         {
             return (Int16) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override int GetInt32(int ordinal)
         {
             return (Int32) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override long GetInt64(int ordinal)
         {
             return (Int64) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override string GetName(int ordinal)
         {
             return popul.Columns[ordinal].Name;
         }
 
+        /// <inheritdoc />
         public override int GetOrdinal(string name)
         {
             return colidx[name];
@@ -178,11 +200,13 @@ namespace Cassandra.Data
             return (string) GetValue(ordinal);
         }
 
+        /// <inheritdoc />
         public override object GetValue(int ordinal)
         {
             return enumerRows.Current[ordinal];
         }
 
+        /// <inheritdoc />
         public override int GetValues(object[] values)
         {
             for (int i = 0; i < enumerRows.Current.Length; i++)
@@ -190,16 +214,19 @@ namespace Cassandra.Data
             return enumerRows.Current.Length;
         }
 
+        /// <inheritdoc />
         public override bool IsDBNull(int ordinal)
         {
             return enumerRows.Current.IsNull(ordinal);
         }
 
+        /// <inheritdoc />
         public override bool NextResult()
         {
             return enumerRows.MoveNext();
         }
 
+        /// <inheritdoc />
         public override bool Read()
         {
             return enumerRows.MoveNext();
