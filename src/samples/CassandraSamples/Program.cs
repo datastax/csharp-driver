98f93f2c8bd834e339c3a9a7cf3a3b47215f7ec6
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-05-21 16:12:42 +0200

    Sample project with time series

---
+++ src/samples/CassandraSamples/Program.cs
@@ -0,0 +1,69 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CassandraSamples
+{
+    class Program
+    {
+        private static ISession _session;
+
+        static void Main(string[] args)
+        {
+            //Create a cluster instance using a builder
+            //You can specify additional options like: credentials, enable SSL, ...
+            var cluster = Cluster.Builder()
+                .AddContactPoint("127.0.0.1")
+                .Build();
+
+            //Create the schema for the sample (out of scope of the samples): TL;DR
+            SchemaHelper.CreateSchema(cluster);
+
+            //Use the cluster to create a Cassandra.Session instance
+            //This session uses the specified keyspace
+            _session = cluster.Connect("driver_samples_kp");
+
+            TimeSeriesExample();
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+
+        public static void TimeSeriesExample()
+        {
+            Console.WriteLine("Executing time series sample");
+
+            var repository = new TemperatureRepository(_session);
+            //Insert some data
+            var weatherStation = "station1";
+            //Trying to simulate the insertion of several rows
+            //with temperature measures
+            for (var i = 0; i < 1000; i++ )
+            {
+                repository.AddTemperature(weatherStation, i / 16M);
+            }
+
+            //Now lets retrieve the temperatures for a given date
+            var rs = repository.GetTemperatureRecords(weatherStation, DateTime.Today);
+            //lets print a few of them
+            Console.WriteLine("Printing the first temperature records (only 20)");
+            var counter = 0;
+            foreach (var row in rs)
+            {
+                Console.Write(row.GetValue<string>("weatherstation_id"));
+                Console.Write("\t");
+                Console.Write(row.GetValue<DateTime>("event_time").ToString("HH:mm:ss.fff"));
+                Console.Write("\t");
+                Console.WriteLine(row.GetValue<decimal>("temperature"));
+                //It is just an example, 20 is enough
+                if (counter++ == 20)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/samples/CassandraSamples/TemperatureRepository.cs
@@ -0,0 +1,60 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CassandraSamples
+{
+    /// <summary>
+    /// Represents an application repository.
+    /// Based on the schema in: http://planetcassandra.org/blog/post/getting-started-with-time-series-data-modeling/
+    /// </summary>
+    public class TemperatureRepository
+    {
+        protected ISession Session { get; set; }
+
+        /// <summary>
+        /// Create a new instance of the repository with the session as a dependency
+        /// </summary>
+        public TemperatureRepository(ISession session)
+        {
+            this.Session = session;
+        }
+
+        /// <summary>
+        /// Add a temperature information for a given weather station
+        /// </summary>
+        public void AddTemperature(string weatherStationId, decimal value)
+        {
+            var insertCql = @"
+                INSERT INTO temperature_by_day 
+                (weatherstation_id, date, event_time, temperature)
+                VALUES
+                (?, ?, ?, ?)";
+           
+            //Create an insert statement
+            var insertStatement = new SimpleStatement(insertCql);
+            //Bind the parameters to the statement
+            insertStatement.Bind(weatherStationId, DateTime.Now.ToString("yyyyMMdd"), DateTime.Now, value);
+            //You can set other options of the statement execution, for example the consistency level.
+            insertStatement.SetConsistencyLevel(ConsistencyLevel.Quorum);
+            //Execute the insert
+            Session.Execute(insertStatement);
+        }
+
+        /// <summary>
+        /// Gets the list of temperature stored for a provided weather station and day.
+        /// </summary>
+        public RowSet GetTemperatureRecords(string weatherStationId, DateTime day)
+        {
+            var selectCql = "SELECT * FROM temperature_by_day WHERE weatherstation_id = ? AND date = ?";
+            //Create a statement
+            var selectStatement = new SimpleStatement(selectCql);
+            //Add the parameters
+            selectStatement.Bind(weatherStationId, DateTime.Now.ToString("yyyyMMdd"));
+            //Execute the select statement
+            return Session.Execute(selectStatement);
+        }
+    }
+}
