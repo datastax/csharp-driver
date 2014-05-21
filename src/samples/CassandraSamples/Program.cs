0e620225af84ba4e609417c092339a6dba927b4a
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-05-21 16:28:48 +0200

    Samples: Added async timeseries sample

--- src/samples/CassandraSamples/Program.cs
+++ src/samples/CassandraSamples/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CassandraSamples
 {
@@ -27,12 +28,18 @@ namespace CassandraSamples
 
             TimeSeriesExample();
 
+            TimeSeriesExampleAsync();
+
             Console.WriteLine("Press enter to exit...");
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Inserts a few records and then retrieves some of them
+        /// </summary>
         public static void TimeSeriesExample()
         {
+            Console.WriteLine("--------------------");
             Console.WriteLine("Executing time series sample");
 
             var repository = new TemperatureRepository(_session);
@@ -49,6 +56,51 @@ namespace CassandraSamples
             var rs = repository.GetTemperatureRecords(weatherStation, DateTime.Today);
             //lets print a few of them
             Console.WriteLine("Printing the first temperature records (only 20)");
+            var counter = 0;
+            foreach (var row in rs)
+            {
+                Console.Write(row.GetValue<string>("weatherstation_id"));
+                Console.Write("\t");
+                Console.Write(row.GetValue<DateTime>("event_time").ToString("HH:mm:ss.fff"));
+                Console.Write("\t");
+                Console.WriteLine(row.GetValue<decimal>("temperature"));
+                //It is just an example, 20 is enough
+                if (counter++ == 20)
+                {
+                    break;
+                }
+            }
+        }
+
+        /// Inserts a few records and then retrieves some of them
+        public static void TimeSeriesExampleAsync()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Executing time series async sample");
+
+            var repository = new TemperatureRepository(_session);
+            //Insert some data
+            var weatherStation = "station1";
+            //Trying to simulate the insertion of several rows
+            //with temperature measures
+            //We will not wait for the insertion to try to insert the next
+            //We will create a task list
+            var insertTaskList = new List<Task>();
+            for (var i = 0; i < 1000; i++)
+            {
+                //It returns a task that is going to be completed when the Cassandra ring acknowledge the insert
+                var task = repository.AddTemperatureAsync(weatherStation, i / 16M);
+                //Maintain the task for later use.
+                insertTaskList.Add(task);
+            }
+
+            //Now lets wait until any of the insert succeeds.
+            Task.WaitAny(insertTaskList.ToArray());
+
+            //Now lets retrieve the temperatures for a given date
+            var rs = repository.GetTemperatureRecords(weatherStation, DateTime.Today);
+            //lets print a few of them
+            Console.WriteLine("Printing the first temperature records (only 20, if available)");
             var counter = 0;
             foreach (var row in rs)
             {
--- src/samples/CassandraSamples/TemperatureRepository.cs
+++ src/samples/CassandraSamples/TemperatureRepository.cs
@@ -3,12 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CassandraSamples
 {
     /// <summary>
     /// Represents an application repository.
     /// Based on the schema in: http://planetcassandra.org/blog/post/getting-started-with-time-series-data-modeling/
+    /// There is almost no code reuse in order to have all the necessary code in one place.
+    /// There are several optimizations that can be made, but are out of scope of this sample.
     /// </summary>
     public class TemperatureRepository
     {
@@ -55,6 +58,30 @@ namespace CassandraSamples
             selectStatement.Bind(weatherStationId, DateTime.Now.ToString("yyyyMMdd"));
             //Execute the select statement
             return Session.Execute(selectStatement);
+        }
+
+        /// <summary>
+        /// Add a temperature information for a given weather station asynchronously
+        /// </summary>
+        public Task AddTemperatureAsync(string weatherStationId, decimal value)
+        {
+            //It is basically the same code as the AddTemperature
+            //Except it returns a Task that already started but didn't finished
+            //The row would not be in Cassandra yet when this method finishes executing.
+            var insertCql = @"
+                INSERT INTO temperature_by_day 
+                (weatherstation_id, date, event_time, temperature)
+                VALUES
+                (?, ?, ?, ?)";
+
+            //Create an insert statement
+            var insertStatement = new SimpleStatement(insertCql);
+            //Bind the parameters to the statement
+            insertStatement.Bind(weatherStationId, DateTime.Now.ToString("yyyyMMdd"), DateTime.Now, value);
+            //You can set other options of the statement execution, for example the consistency level.
+            insertStatement.SetConsistencyLevel(ConsistencyLevel.Quorum);
+            //Execute the insert
+            return Session.ExecuteAsync(insertStatement);
         }
     }
 }
