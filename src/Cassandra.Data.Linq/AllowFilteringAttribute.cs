d1a188fcd2447bd871caac32436ab499cb5ca00b
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-06-18 12:07:37 +0200

    Linq: Added Allow Filtering performance note

--- src/Cassandra.Data.Linq/AllowFilteringAttribute.cs
+++ src/Cassandra.Data.Linq/AllowFilteringAttribute.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Cassandra.Data.Linq
 {
+    /// <summary>
+    /// The ALLOW FILTERING option allows to explicitly allow queries that require filtering. 
+    /// Please note that a query using ALLOW FILTERING may thus have unpredictable performance (for the definition above), i.e. even a query that selects a handful of records may exhibit performance that depends on the total amount of data stored in the cluster.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
     public class AllowFilteringAttribute : Attribute
     {
--- src/Cassandra.Data.Linq/CqlQueryExtensions.cs
+++ src/Cassandra.Data.Linq/CqlQueryExtensions.cs
@@ -206,6 +206,10 @@ namespace Cassandra.Data.Linq
             return ret;
         }
 
+        /// <summary>
+        /// The ALLOW FILTERING option allows to explicitly allow queries that require filtering. 
+        /// Please note that a query using ALLOW FILTERING may thus have unpredictable performance (for the definition above), i.e. even a query that selects a handful of records may exhibit performance that depends on the total amount of data stored in the cluster.
+        /// </summary>
         public static CqlQuery<TSource> AllowFiltering<TSource>(this CqlQuery<TSource> source)
         {
             var ret = (CqlQuery<TSource>)source.Provider.CreateQuery<TSource>(Expression.Call(
