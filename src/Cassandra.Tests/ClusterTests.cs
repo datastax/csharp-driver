cb59a0becb1b57ace78ebb2280be908c2a6f8eda
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-11-03 16:59:10 +0100

    Add unit tests for Cluster Dispose after init error and host name resolving

--- src/Cassandra.Tests/ClusterTests.cs
+++ src/Cassandra.Tests/ClusterTests.cs
@@ -53,5 +53,16 @@ namespace Cassandra.Tests
             Assert.Throws<NoHostAvailableException>(() => cluster.Connect());
             Assert.Throws<NoHostAvailableException>(() => cluster.Connect("sample_ks"));
         }
+
+        [Test]
+        public void ClusterIsDisposableAfterInitError()
+        {
+            const string ip = "127.100.100.100";
+            var cluster = Cluster.Builder()
+             .AddContactPoint(ip)
+             .Build();
+            Assert.Throws<NoHostAvailableException>(() => cluster.Connect());
+            Assert.DoesNotThrow(cluster.Dispose);
+        }
     }
 }
