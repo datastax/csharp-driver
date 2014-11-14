5c79a87081a9988ae573d8c078d5bf85b56d6260
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-11-14 14:04:41 +0100

    Test: mapping fetch async prepared called just once for the same query

--- src/Cassandra.Tests/Mapping/FetchTests.cs
+++ src/Cassandra.Tests/Mapping/FetchTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Tasks;
 using Cassandra.Mapping;
 using Cassandra.Mapping.FluentMapping;
 using Cassandra.Tests.Mapping.Pocos;
@@ -14,17 +16,20 @@ namespace Cassandra.Tests.Mapping
     [TestFixture]
     public class FetchTests
     {
-        private ICqlClient GetMappingClient(RowSet rowset)
+        private ICqlClient GetMappingClient(RowSet rowset = null, Mock<ISession> sessionMock = null)
         {
-            var sessionMock = new Mock<ISession>(MockBehavior.Strict);
-            sessionMock
-                .Setup(s => s.ExecuteAsync(It.IsAny<BoundStatement>()))
-                .Returns(TaskHelper.ToTask(rowset))
-                .Verifiable();
-            sessionMock
-                .Setup(s => s.PrepareAsync(It.IsAny<string>()))
-                .Returns(TaskHelper.ToTask(new PreparedStatement(null, null, null, null)))
-                .Verifiable();
+            if (sessionMock == null)
+            {
+                sessionMock = new Mock<ISession>(MockBehavior.Strict);
+                sessionMock
+                    .Setup(s => s.ExecuteAsync(It.IsAny<BoundStatement>()))
+                    .Returns(TaskHelper.ToTask(rowset))
+                    .Verifiable();
+                sessionMock
+                    .Setup(s => s.PrepareAsync(It.IsAny<string>()))
+                    .Returns(TaskHelper.ToTask(new PreparedStatement(null, null, null, null)))
+                    .Verifiable();
+            }
             var mappingClient = CqlClientConfiguration
                 .ForSession(sessionMock.Object)
                 .BuildCqlClient();
@@ -34,7 +39,6 @@ namespace Cassandra.Tests.Mapping
         [Test]
         public void FetchAsync_Pocos_WithCql_Empty()
         {
-            var sessionMock = new Mock<ISession>(MockBehavior.Strict);
             var rowset = new RowSet();
             var mappingClient = GetMappingClient(rowset);
             var userTask = mappingClient.FetchAsync<PlainUser>("SELECT * FROM users");
@@ -54,6 +58,39 @@ namespace Cassandra.Tests.Mapping
             var users = userTask.Result;
             Assert.NotNull(users);
             CollectionAssert.AreEqual(usersExpected, users, new TestHelper.PropertyComparer());
+        }
+
+        [Test]
+        public void FetchAsync_Pocos_Prepares_Just_Once()
+        {
+            const int times = 100;
+            var users = TestDataHelper.GetUserList();
+            var sessionMock = new Mock<ISession>(MockBehavior.Strict);
+            sessionMock
+                .Setup(s => s.ExecuteAsync(It.IsAny<BoundStatement>()))
+                .Returns(() => TaskHelper.ToTask(TestDataHelper.GetUsersRowSet(users)))
+                .Verifiable();
+            sessionMock
+                .Setup(s => s.PrepareAsync(It.IsAny<string>()))
+                .Returns(TaskHelper.ToTask(new PreparedStatement(null, null, null, null)))
+                .Verifiable();
+            var mappingClient = GetMappingClient(null, sessionMock);
+            var taskList = new List<Task<List<PlainUser>>>();
+            for (var i = 0; i < times; i++)
+            {
+                var t = mappingClient.FetchAsync<PlainUser>("SELECT * FROM users");
+                taskList.Add(t);
+            }
+
+            Task.WaitAll(taskList.Select(t => (Task)t).ToArray(), 5000);
+            Assert.True(taskList.All(t => t.Result.Count == 10));
+            sessionMock.Verify();
+            //Prepare should be called just once
+            sessionMock
+                .Verify(s => s.PrepareAsync(It.IsAny<string>()), Times.Once());
+            //ExecuteAsync should be called the exact number of times
+            sessionMock
+                .Verify(s => s.ExecuteAsync(It.IsAny<BoundStatement>()), Times.Exactly(times));
         }
     }
 }
