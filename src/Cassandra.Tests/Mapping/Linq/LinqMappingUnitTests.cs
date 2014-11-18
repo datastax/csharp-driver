a453060cc98c493d3e6025cf15a811ff185feb17
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-11-18 14:51:41 +0100

    Basic linq mapping unit test

---
+++ src/Cassandra.Tests/Mapping/Linq/LinqMappingUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cassandra.Data.Linq;
+using Cassandra.Tests.Mapping.Pocos;
+using Cassandra.Tests.Mapping.TestData;
+using Moq;
+using NUnit.Framework;
+
+namespace Cassandra.Tests.Mapping.Linq
+{
+    public class LinqMappingUnitTests : MappingTestBase
+    {
+        [Test]
+        public void Linq_CqlQueryBase_Execute_Empty_RowSet()
+        {
+            var sessionMock = new Mock<ISession>(MockBehavior.Strict);
+            sessionMock
+                .Setup(s => s.ExecuteAsync(It.IsAny<SimpleStatement>()))
+                .Returns(TaskHelper.ToTask(new RowSet()))
+                .Verifiable();
+            sessionMock.Setup(s => s.BinaryProtocolVersion).Returns(2);
+            var table = SessionExtensions.GetTable<PlainUser>(sessionMock.Object);
+            var entities = table.Where(a => a.UserId == Guid.Empty).Execute();
+            Assert.AreEqual(0, entities.Count());
+        }
+
+        [Test]
+        public void Linq_CqlQueryBase_Execute_Maps_Rows()
+        {
+            var usersExpected = TestDataHelper.GetUserList();
+            var sessionMock = new Mock<ISession>(MockBehavior.Strict);
+            sessionMock
+                .Setup(s => s.ExecuteAsync(It.IsAny<SimpleStatement>()))
+                .Returns(TaskHelper.ToTask(TestDataHelper.GetUsersRowSet(usersExpected)))
+                .Verifiable();
+            sessionMock.Setup(s => s.BinaryProtocolVersion).Returns(2);
+            var table = SessionExtensions.GetTable<PlainUser>(sessionMock.Object);
+            var users = table.Execute().ToList();
+            CollectionAssert.AreEqual(usersExpected, users, new TestHelper.PropertyComparer());
+        }
+    }
+}
