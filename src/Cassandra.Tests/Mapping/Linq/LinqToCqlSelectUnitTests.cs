4a5bb394b17f42b4582da5e4ca2fb70e9293076c
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-11-28 14:45:28 +0100

    Test select projections and order by clause generation

--- src/Cassandra.Tests/Mapping/Linq/LinqToCqlSelectUnitTests.cs
+++ src/Cassandra.Tests/Mapping/Linq/LinqToCqlSelectUnitTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Cassandra.Data.Linq;
 using Cassandra.Mapping.FluentMapping;
 using Cassandra.Tests.Mapping.Pocos;
+using Cassandra.Tests.Mapping.TestData;
 using Moq;
 using NUnit.Framework;
 
@@ -13,12 +14,16 @@ namespace Cassandra.Tests.Mapping.Linq
     [TestFixture]
     public class LinqToCqlSelectUnitTests
     {
-        private static ISession GetSession(Action<string, object[]> callback)
+        private static ISession GetSession(Action<string, object[]> callback, RowSet rs = null)
         {
+            if (rs == null)
+            {
+                rs = new RowSet();
+            }
             var sessionMock = new Mock<ISession>(MockBehavior.Strict);
             sessionMock
                 .Setup(s => s.ExecuteAsync(It.IsAny<BoundStatement>()))
-                .Returns(() => TaskHelper.ToTask(new RowSet()))
+                .Returns(() => TaskHelper.ToTask(rs))
                 .Callback<BoundStatement>(stmt => callback(stmt.PreparedStatement.Cql, stmt.QueryValues))
                 .Verifiable();
             sessionMock
@@ -56,6 +61,79 @@ namespace Cassandra.Tests.Mapping.Linq
             table.AllowFiltering().Execute();
             Assert.AreEqual("SELECT * FROM values ALLOW FILTERING", query);
             Assert.AreEqual(0, parameters.Length);
+        }
+
+        [Test]
+        public void Select_Project_To_New_Type_Constructor()
+        {
+            string query = null;
+            object[] parameters = null;
+            var session = GetSession((q, v) =>
+            {
+                query = q;
+                parameters = v;
+            }, TestDataHelper.CreateMultipleValuesRowSet(new[] { "val", "id" }, new[] { 1, 200 }));
+            var map = new Map<AllTypesEntity>()
+                .ExplicitColumns()
+                .Column(t => t.DoubleValue, cm => cm.WithName("val"))
+                .Column(t => t.IntValue, cm => cm.WithName("id"))
+                .PartitionKey(t => t.UuidValue)
+                .TableName("tbl1");
+            var table = session.GetTable<AllTypesEntity>(map);
+            (from t in table where t.IntValue == 200 select new Tuple<double, int>(t.DoubleValue, t.IntValue)).Execute();
+            Assert.AreEqual("SELECT val, id FROM tbl1 WHERE id = ?", query);
+            CollectionAssert.AreEqual(parameters, new object[] { 200 });
+        }
+
+        [Test]
+        public void Select_Project_To_New_Type()
+        {
+            string query = null;
+            object[] parameters = null;
+            var session = GetSession((q, v) =>
+            {
+                query = q;
+                parameters = v;
+            }, TestDataHelper.CreateMultipleValuesRowSet(new[] { "val", "id" }, new[] { 1, 200 }));
+            var map = new Map<AllTypesEntity>()
+                .ExplicitColumns()
+                .Column(t => t.DoubleValue, cm => cm.WithName("val"))
+                .Column(t => t.IntValue, cm => cm.WithName("id"))
+                .PartitionKey(t => t.UuidValue)
+                .TableName("tbl1");
+            var table = session.GetTable<AllTypesEntity>(map);
+            (from t in table where t.IntValue == 200 select new PlainUser {Age = t.IntValue}).Execute();
+            Assert.AreEqual("SELECT id FROM tbl1 WHERE id = ?", query);
+            CollectionAssert.AreEqual(parameters, new object[] { 200 });
+        }
+
+        [Test]
+        public void Select_Expression_OrderBy_Test()
+        {
+            string query = null;
+            object[] parameters = null;
+            var session = GetSession((q, v) =>
+            {
+                query = q;
+                parameters = v;
+            });
+            var map = new Map<AllTypesEntity>()
+                .ExplicitColumns()
+                .Column(t => t.DoubleValue, cm => cm.WithName("val"))
+                .Column(t => t.UuidValue, cm => cm.WithName("id"))
+                .Column(t => t.DecimalValue, cm => cm.WithName("val2"))
+                .Column(t => t.StringValue, cm => cm.WithName("string_val"))
+                .PartitionKey(t => t.UuidValue)
+                .TableName("tbl1");
+            var id = Guid.NewGuid();
+            var table = session.GetTable<AllTypesEntity>(map);
+            (from t in table where t.UuidValue == id orderby t.StringValue descending select t).Execute();
+            Assert.AreEqual("SELECT * FROM tbl1 WHERE id = ? ORDER BY string_val DESC", query);
+            CollectionAssert.AreEqual(parameters, new object[] { id });
+
+            (from t in table where t.UuidValue == id orderby t.StringValue select t).Execute();
+            Assert.AreEqual("SELECT * FROM tbl1 WHERE id = ? ORDER BY string_val", query);
+            CollectionAssert.AreEqual(parameters, new object[] { id });
         }
     }
 }
