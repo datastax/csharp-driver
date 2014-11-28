4f7c593efe5d8c2ab6bbe33396477420b65a2c1a
Jorge Bay Gondra <jorge.gondra@datastax.com>
2014-11-28 16:37:52 +0100

    Test: Linq Update multiple where calls

--- src/Cassandra.Tests/Mapping/Linq/LinqToCqlUpdateUnitTests.cs
+++ src/Cassandra.Tests/Mapping/Linq/LinqToCqlUpdateUnitTests.cs
@@ -36,10 +36,40 @@ namespace Cassandra.Tests.Mapping.Linq
                 .Where(t => t.UuidValue == id)
                 .Select(t => new AllTypesEntity { StringValue = "Billy the Vision", DecimalValue = 10M })
                 .Update()
-                .SetTTL(60*60)
+                .SetTTL(60 * 60)
                 .Execute();
             Assert.AreEqual("UPDATE tbl1 USING TTL ? SET string_val = ?, val2 = ? WHERE id = ?", query);
-            CollectionAssert.AreEqual(new object[] {60 * 60, "Billy the Vision", 10M, id}, parameters);
+            CollectionAssert.AreEqual(new object[] { 60 * 60, "Billy the Vision", 10M, id }, parameters);
+        }
+
+        [Test]
+        public void Update_Multiple_Where_Test()
+        {
+            string query = null;
+            object[] parameters = null;
+            var session = GetSession((q, v) =>
+            {
+                query = q;
+                parameters = v;
+            });
+            var map = new Map<AllTypesEntity>()
+                .ExplicitColumns()
+                .Column(t => t.DoubleValue, cm => cm.WithName("val"))
+                .Column(t => t.UuidValue, cm => cm.WithName("id"))
+                .Column(t => t.DecimalValue, cm => cm.WithName("val2"))
+                .Column(t => t.StringValue, cm => cm.WithName("string_val"))
+                .PartitionKey(t => t.UuidValue)
+                .TableName("tbl1");
+            var id = Guid.NewGuid();
+            var table = session.GetTable<AllTypesEntity>(map);
+            table
+                .Where(t => t.UuidValue == id)
+                .Where(t => t.DecimalValue > 20M)
+                .Select(t => new AllTypesEntity { StringValue = "Billy the Vision" })
+                .Update()
+                .Execute();
+            Assert.AreEqual("UPDATE tbl1 SET string_val = ? WHERE id = ? AND val2 > ?", query);
+            CollectionAssert.AreEqual(new object[] { "Billy the Vision", id, 20M}, parameters);
         }
 
         [Test]
