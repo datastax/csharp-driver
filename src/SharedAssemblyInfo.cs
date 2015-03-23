be7fbc72195b759399fb70c848980ea8431636cd
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-03-23 12:04:35 +0100

    Version bump to 2.1.7

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -12,8 +12,8 @@
 [assembly: System.Reflection.AssemblyProduct("Cassandra .NET Driver")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2015 by DataStax")]
-[assembly: System.Reflection.AssemblyVersion("2.1.6")]
-[assembly: System.Reflection.AssemblyInformationalVersion("2.1.6 (git 0dfca47)")]
-[assembly: System.Reflection.AssemblyFileVersion("2.1.6.0")]
+[assembly: System.Reflection.AssemblyVersion("2.1.7")]
+[assembly: System.Reflection.AssemblyInformationalVersion("2.1.7 (git 8d374b0)")]
+[assembly: System.Reflection.AssemblyFileVersion("2.1.7.0")]
 
 
