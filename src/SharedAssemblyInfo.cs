dba6f4e14f9d7d91adc1eb1037d1391ffe99713e
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-04-08 11:09:51 +0200

    Version bump

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -13,7 +13,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2015 by DataStax")]
 [assembly: System.Reflection.AssemblyVersion("2.5")]
-[assembly: System.Reflection.AssemblyInformationalVersion("2.5.1 (git 509cd6d)")]
-[assembly: System.Reflection.AssemblyFileVersion("2.5.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("2.5.2 (git 8a3e75c)")]
+[assembly: System.Reflection.AssemblyFileVersion("2.5.2.0")]
 
 
