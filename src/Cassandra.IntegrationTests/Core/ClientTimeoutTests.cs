e3ddb26aff7d4013bed391238524aa24f77d8fa1
Kishan Karunaratne <kishan@karu.io>
2015-08-27 18:36:38 -0700

    [CSHARP-332] Integration tests for per-host request timeout

--- src/Cassandra.IntegrationTests/Core/ClientTimeoutTests.cs
+++ src/Cassandra.IntegrationTests/Core/ClientTimeoutTests.cs
@@ -148,5 +148,35 @@ namespace Cassandra.IntegrationTests.Core
                 Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
             }
         }
+
+        /// Tests a NoHostAvailableException is raised when all hosts down with read timeout
+        ///
+        /// Should_Throw_NoHostAvailableException_When_All_Hosts_Down tests that the driver quickly throws a NoHostAvailableException 
+        /// when all nodes in the cluster is down, given a set ReadTimeoutMillis of 3 seconds.
+        ///
+        /// @since 2.7.0
+        /// @jira_ticket CSHARP-332
+        /// @expected_result A NoHostAvailableException should be raised after 3 seconds.
+        ///
+        /// @test_category connection:timeout
+        [Test]
+        public void Should_Throw_NoHostAvailableException_When_All_Hosts_Down()
+        {
+            var testCluster = TestClusterManager.GetNonShareableTestCluster(2, 1, true, false);
+            var socketOptions = new SocketOptions().SetReadTimeoutMillis(3000);
+            var builder = Cluster.Builder().AddContactPoint(testCluster.InitialContactPoint)
+                .WithSocketOptions(socketOptions);
+            using (var cluster = builder.Build())
+            {
+                var session = cluster.Connect();
+                //warmup
+                TestHelper.Invoke(() => session.Execute("SELECT key FROM system.local"), 10);
+                testCluster.PauseNode(1);
+                testCluster.PauseNode(2);
+                Assert.Throws<NoHostAvailableException>(() => session.Execute("SELECT key FROM system.local"));
+                testCluster.ResumeNode(1);
+                testCluster.ResumeNode(2);
+            }
+        }
     }
 }
