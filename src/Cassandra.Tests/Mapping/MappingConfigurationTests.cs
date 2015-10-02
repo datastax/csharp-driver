e0c9f4fa07c7f5a44ff00e8a2e7ec62f96cd5b86
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-10-02 10:37:08 +0200

    Mapper Test: setting a new TypeConverter triggers a new MapperFactory instance

---
+++ src/Cassandra.Tests/Mapping/MappingConfigurationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cassandra.Mapping;
+using Cassandra.Mapping.TypeConversion;
+using NUnit.Framework;
+
+namespace Cassandra.Tests.Mapping
+{
+    [TestFixture]
+    public class MappingConfigurationTests
+    {
+        [Test]
+        public void ConvertTypesUsing_Creates_Uses_MapperFactory_Instance()
+        {
+            var config = new MappingConfiguration();
+            var originalMapperFactory = config.MapperFactory;
+            //the mapper factory remains the same
+            Assert.AreSame(originalMapperFactory, config.MapperFactory);
+            config.ConvertTypesUsing(new DefaultTypeConverter());
+            //New instance of the mapper factory
+            Assert.AreNotSame(originalMapperFactory, config.MapperFactory);
+        }
+    }
+}
