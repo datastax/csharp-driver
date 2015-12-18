2d751d162004e2f4d2beb9935a2d90f5d30769f0
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2015-12-18 15:33:09 +0100

    HostConnectionPool: Schedule next reconnection attempt without checking delay passed.
    Remove pool semaphore.
    Use pool method to create first only and other to increase to core and max.
    Reconnection test with multiple nodes: Create a new test fixture of reconnection tests for the category "short". Moved some existing test from PoolTests to the new test fixture.

--- src/Cassandra.IntegrationTests/Core/PoolTests.cs
+++ src/Cassandra.IntegrationTests/Core/PoolTests.cs
@@ -241,32 +241,6 @@ namespace Cassandra.IntegrationTests.Core
             }
         }
 
-        /// <summary>
-        /// Validates that the client adds the newly bootstrapped node and eventually queries from it
-        /// </summary>
-        [Test]
-        public void BootstrappedNode()
-        {
-            var testCluster = TestClusterManager.GetNonShareableTestCluster(1, 1, true, false);
-            using (var cluster = Cluster.Builder().AddContactPoint(testCluster.InitialContactPoint).Build())
-            {
-                var session = cluster.Connect();
-                testCluster.BootstrapNode(2);
-                var queried = false;
-                for (var i = 0; i < 10; i++)
-                {
-                    var rs = session.Execute("SELECT key FROM system.local");
-                    if (rs.Info.QueriedHost.Address.ToString() == testCluster.ClusterIpPrefix + 2)
-                    {
-                        queried = true;
-                        break;
-                    }
-                    Thread.Sleep(1000);
-                }
-                Assert.True(queried, "Newly bootstrapped node should be queried");
-            }
-        }
-
         [Test]
         public void InitialKeyspaceRaceTest()
         {
@@ -462,98 +436,6 @@ namespace Cassandra.IntegrationTests.Core
             Assert.True(waitHandle.WaitOne(waitTime), "Wait time passed but it was not signaled");
             t.Dispose();
             Assert.AreEqual(4, connectionAttempts);
-        }
-
-        /// Tests that reconnection attempts are made automatically in the background
-        ///
-        /// Reconnection_Attempts_Are_Made_In_The_Background tests that the driver automatically reschedules host
-        /// reconnections using timers in the background. It first creates a Cassandra cluster with 2 nodes, and verifies
-        /// that the control connection is created against the first host. It then manually sets the 2nd node as down (even
-        /// though it is still up), verifying that the driver see that host 2 as down. Finally it waits 5 seconds for the 
-        /// timer-based reconnection policy to kick in, and verifies that the driver sees the 2nd host as back up.
-        ///
-        /// @since 2.7.0
-        /// @jira_ticket CSHARP-280
-        /// @expected_result Host 2 should be automatically reconnected in the background after being set as down
-        ///
-        /// @test_assumptions
-        ///    - A Cassandra cluster with 2 nodes
-        /// @test_category connection:reconnection
-        [Test]
-        public void Reconnection_Attempts_Are_Made_In_The_Background()
-        {
-            var testCluster = TestClusterManager.GetNonShareableTestCluster(2, 1, true, false);
-
-            var cluster = Cluster.Builder()
-                                 .AddContactPoint(testCluster.InitialContactPoint)
-                                 .WithPoolingOptions(
-                                     new PoolingOptions()
-                                         .SetCoreConnectionsPerHost(HostDistance.Local, 2)
-                                         .SetHeartBeatInterval(0))
-                                 .WithReconnectionPolicy(new ConstantReconnectionPolicy(2000))
-                                 .Build();
-            var session = (Session)cluster.Connect();
-            TestHelper.Invoke(() => session.Execute("SELECT * FROM system.local"), 10);
-            var host = cluster.AllHosts().First(h => TestHelper.GetLastAddressByte(h) == 2);
-            // Check that the control connection is connected to another host
-            Assert.AreNotEqual(cluster.Metadata.ControlConnection.Address, host.Address);
-            Assert.True(host.IsUp);
-            Trace.TraceInformation("Setting host 2 of 2 as down");
-            host.SetDown();
-            Assert.False(host.IsUp);
-            Trace.TraceInformation("Waiting for 5 seconds");
-            Thread.Sleep(5000);
-            Assert.True(host.IsUp);
-        }
-
-        /// Tests that reconnection attempts are made multiple times in the background
-        ///
-        /// Reconnection_Attempted_Multiple_Times tests that the driver automatically reschedules host reconnections using 
-        /// timers in the background multiple times. It first creates a Cassandra cluster with a single node, and verifies
-        /// that the driver considers it as up. It then stops the single node and verifies that the driver considers the node
-        /// as down, by both executing a query and retreiving a NoHostAvailableException, and checking the host status manually.
-        /// It then waits 15 seconds before verifying once more that the host is seen as down by the driver. Finally it restarts
-        /// the single node, waits 5 seconds for the timer-based reconnection policy to kick in, and verifies that the driver sees 
-        /// the host as back up.
-        ///
-        /// @since 2.7.0
-        /// @jira_ticket CSHARP-280
-        /// @expected_result The host should be attempted to be reconnected multiple times in the background
-        ///
-        /// @test_assumptions
-        ///    - A Cassandra cluster with a single node
-        /// @test_category connection:reconnection
-        [Test]
-        public void Reconnection_Attempted_Multiple_Times()
-        {
-            var testCluster = TestClusterManager.GetNonShareableTestCluster(1, 1, true, false);
-
-            using (var cluster = Cluster.Builder()
-                                        .AddContactPoint(testCluster.InitialContactPoint)
-                                        .WithPoolingOptions(
-                                            new PoolingOptions()
-                                                .SetCoreConnectionsPerHost(HostDistance.Local, 2))
-                                        .WithReconnectionPolicy(new ConstantReconnectionPolicy(2000))
-                                        .Build())
-            {
-                var session = (Session)cluster.Connect();
-                TestHelper.Invoke(() => session.Execute("SELECT * FROM system.local"), 10);
-                var host = cluster.AllHosts().First();
-                Assert.True(host.IsUp);
-                Trace.TraceInformation("Stopping node 1 of 1");
-                testCluster.Stop(1);
-                // Make sure the node is considered down
-                Assert.Throws<NoHostAvailableException>(() => session.Execute("SELECT * FROM system.local"));
-                Assert.False(host.IsUp);
-                Trace.TraceInformation("Waiting for 15 seconds");
-                Thread.Sleep(15000);
-                Assert.False(host.IsUp);
-                Trace.TraceInformation("Restarting node 1 of 1");
-                testCluster.Start(1);
-                Trace.TraceInformation("Waiting for 5 seconds");
-                Thread.Sleep(5000);
-                Assert.True(host.IsUp);
-            }
         }
 
         [Test]
--- src/Cassandra.Tests/HostTests.cs
+++ src/Cassandra.Tests/HostTests.cs
@@ -16,30 +16,6 @@ namespace Cassandra.Tests
         private static readonly IPEndPoint Address = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1000);
 
         [Test]
-        public void SetDown_Should_Get_The_Next_Delay_Once_The_Time_Passes()
-        {
-            var policy = new CountReconnectionPolicy(3000);
-            var host = new Host(Address, policy);
-            TestHelper.ParallelInvoke(() =>
-            {
-                host.SetDown();
-            }, 5);
-            //Should call the Schedule#NextDelayMs()
-            Assert.AreEqual(1, policy.CallsCount);
-            //SetDown should do nothing as the time has not passed
-            host.SetDown();
-            Assert.AreEqual(1, policy.CallsCount);
-            Thread.Sleep(3000);
-            Assert.True(host.IsConsiderablyUp);
-            //The nextUpTime passed
-            TestHelper.ParallelInvoke(() =>
-            {
-                host.SetDown();
-            }, 5);
-            Assert.AreEqual(2, policy.CallsCount);
-        }
-
-        [Test]
         public void BringUpIfDown_Should_Allow_Multiple_Concurrent_Calls()
         {
             var policy = new CountReconnectionPolicy(100);
@@ -56,46 +32,19 @@ namespace Cassandra.Tests
         }
 
         [Test]
-        public void HostConnectionPool_MinInFlight_Returns_The_Min_Inflight_From_Two_Connections()
+        public void BringUpIfDown_Resets_Attempt_Reconnection_Flag()
         {
-            var connections = new[]
-            {
-                GetConnectionMock(0),
-                GetConnectionMock(1),
-                GetConnectionMock(1),
-                GetConnectionMock(10),
-                GetConnectionMock(1),
-            };
-            var index = 1;
-            var c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 2);
-            Assert.AreSame(connections[2], c);
-            c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 3);
-            //previous had less in flight
-            Assert.AreSame(connections[2], c);
-            c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 4);
-            Assert.AreSame(connections[4], c);
-            c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 5);
-            Assert.AreSame(connections[0], c);
-            c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 6);
-            Assert.AreSame(connections[0], c);
-            index = 9;
-            c = HostConnectionPool.MinInFlight(connections, ref index);
-            Assert.AreEqual(index, 10);
-            Assert.AreSame(connections[0], c);
-        }
-
-        private static Connection GetConnectionMock(int inflight)
-        {
-            var config = new Configuration();
-            config.BufferPool = new Microsoft.IO.RecyclableMemoryStreamManager();
-            var connectionMock = new Mock<Connection>(MockBehavior.Strict, (byte) 4, Address, config);
-            connectionMock.Setup(c => c.InFlight).Returns(inflight);
-            return connectionMock.Object;
+            var policy = new CountReconnectionPolicy(long.MaxValue);
+            var host = new Host(Address, policy);
+            host.SetDown();
+            Assert.True(host.SetAttemptingReconnection());
+            //Next times it should be false
+            Assert.False(host.SetAttemptingReconnection());
+            Assert.False(host.SetAttemptingReconnection());
+            Assert.False(host.SetAttemptingReconnection());
+            host.BringUpIfDown();
+            //next time should be allowed
+            Assert.True(host.SetAttemptingReconnection());
         }
 
         private class CountReconnectionPolicy : IReconnectionPolicy
