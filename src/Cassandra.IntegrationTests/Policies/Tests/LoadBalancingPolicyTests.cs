c4640617ac06de3e5491745e4edb0d43298fe007
Kishan Karunaratne <kishan@karu.io>
2016-01-07 21:56:27 -0800

    [CSHARP-385] Tests for TokenMap rebuild evaluation in non-existent DC

--- src/Cassandra.IntegrationTests/Policies/Tests/LoadBalancingPolicyTests.cs
+++ src/Cassandra.IntegrationTests/Policies/Tests/LoadBalancingPolicyTests.cs
@@ -654,5 +654,56 @@ namespace Cassandra.IntegrationTests.Policies.Tests
             policyTestTools.AssertQueried(testCluster.ClusterIpPrefix + nodePositionToNotDecommission + ":" + DefaultCassandraPort, 12);
         }
 
+        /// Tests that the TokenMap can be rebulit using an existing keyspace's RF, even with a decommisioned DC
+        ///
+        /// TokenMap_Rebuild_With_Decommissioned_DC_Existing_RF tests that a keyspace with a non-existent DC in the replication
+        /// options can be connected. It first creates a 2 datacenter cluster with 1 node in each. It then connects to this cluster
+        /// using dc1 and creates a multi-dc schema which has a replication factor of 1 in each dc. It performs a simple insertion
+        /// and verifies that the reads are performed from dc1 as expected. It then decommissions dc1 by removing its only node, causing
+        /// the keyspace replication factor to point to a non-existing "dc1". It then verifies that the driver is able to reconnect to the 
+        /// cluster via dc2. Finally it performs some simple reads to verify that the data is read from dc2.
+        ///
+        /// @since 3.0.1
+        /// @jira_ticket CSHARP-385
+        /// @expected_result TokenMap is successfully rebuilt with decomissioned DC, with existing RFs.
+        ///
+        /// @test_category control_connection
+        [Test]
+        public void TokenMap_Rebuild_With_Decommissioned_DC_Existing_RF()
+        {
+            // Create a 2dc:1node each cluster
+            var clusterOptions = new TestClusterOptions();
+            clusterOptions.Dc2NodeLength = 1;
+            var testCluster = TestClusterManager.CreateNew(1, clusterOptions);
+
+            testCluster.Builder = Cluster.Builder().AddContactPoint("127.0.0.1").WithLoadBalancingPolicy(new DCAwareRoundRobinPolicy("dc1"));
+            testCluster.Cluster = testCluster.Builder.Build();
+            testCluster.Session = testCluster.Cluster.Connect();
+
+            PolicyTestTools policyTestTools = new PolicyTestTools();
+            // Create a ks with RF = dc1:1, dc2:1
+            policyTestTools.CreateMultiDcSchema(testCluster.Session, 1, 1);
+            policyTestTools.InitPreparedStatement(testCluster, 12, false, ConsistencyLevel.All);
+            // Replicas now in each node in each dc
+
+            policyTestTools.Query(testCluster, 12, false);
+            policyTestTools.AssertQueried(testCluster.ClusterIpPrefix + "1:" + DefaultCassandraPort, 12);
+            policyTestTools.AssertQueried(testCluster.ClusterIpPrefix + "2:" + DefaultCassandraPort, 0);
+            testCluster.Cluster.Shutdown();
+
+            testCluster.DecommissionNode(1);
+            // dc1 no longer has any hosts
+
+            testCluster.Builder = Cluster.Builder().AddContactPoint("127.0.0.2").WithLoadBalancingPolicy(new DCAwareRoundRobinPolicy("dc2"));
+            testCluster.Cluster = testCluster.Builder.Build();
+            // Should be able to connect and rebuild token map
+            testCluster.Session = testCluster.Cluster.Connect(policyTestTools.DefaultKeyspace);
+
+            policyTestTools.ResetCoordinators();
+            policyTestTools.Query(testCluster, 12, false);
+            policyTestTools.AssertQueried(testCluster.ClusterIpPrefix + "1:" + DefaultCassandraPort, 0);
+            policyTestTools.AssertQueried(testCluster.ClusterIpPrefix + "2:" + DefaultCassandraPort, 12);
+            testCluster.Cluster.Shutdown();
+        }
     }
 }
