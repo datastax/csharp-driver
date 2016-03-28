d9bfe1e7e01b1476fb5f32a312a19d937df7fb13
Collin Sauve <collinsauve@gmail.com>
2016-03-28 11:48:53 -0400

    remove usage of null-conditional operators

--- src/Cassandra/Mapping/Mapper.cs
+++ src/Cassandra/Mapping/Mapper.cs
@@ -234,7 +234,8 @@ namespace Cassandra.Mapping
             var values = getBindValues(poco);
             object[] queryParameters;
             //generate INSERT query based on null values (if insertNulls set)
-            var cql = _cqlGenerator.GenerateInsert<T>(insertNulls, values, out queryParameters, true, ttl, queryOptions?.Timestamp);
+            var timestamp = queryOptions == null ? null : queryOptions.Timestamp;
+            var cql = _cqlGenerator.GenerateInsert<T>(insertNulls, values, out queryParameters, true, ttl, timestamp);
             return ExecuteAsyncAndAdapt(
                 Cql.New(cql, queryParameters, queryOptions ?? CqlQueryOptions.None),
                 (stmt, rs) => AppliedInfo<T>.FromRowSet(_mapperFactory, cql, rs));
