e86f96c81d4b9ce6356ce25735c5c3e6437da311
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2016-05-27 13:47:10 +0200

    Move Graph Set() methods to IGraphStatement interface

--- src/Dse/Graph/GraphStatement.cs
+++ src/Dse/Graph/GraphStatement.cs
@@ -41,14 +41,10 @@ namespace Dse.Graph
         /// <inheritdoc />
         public int ReadTimeoutMillis { get; protected set; }
 
-        /// <summary>
-        /// Gets the default timestamp associated with this query.
-        /// </summary>
+        /// <inheritdoc />
         public DateTimeOffset? Timestamp { get; protected set; }
 
-        /// <summary>
-        /// Gets the IStatement for this GraphStatement instance.
-        /// </summary>
+        /// <inheritdoc />
         internal abstract IStatement GetIStatement(GraphOptions options);
 
         /// <summary>
@@ -69,124 +65,71 @@ namespace Dse.Graph
                    && Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false);
         }
 
-        /// <summary>
-        /// Sets the consistency level to use for this statement.
-        /// <para>
-        /// This setting will affect the general consistency when executing the gremlin query. However
-        /// executing a gremlin query on the server side is going to involve the execution of CQL queries to the 
-        /// persistence engine that is Cassandra. Those queries can be both reads and writes and both will have a
-        /// settable consistency level. Setting only this property will indicate to the server to use this consistency
-        /// level for both reads and writes in Cassandra. Read or write consistency level can be set separately with
-        /// respectively
-        /// <see cref="SetGraphReadConsistencyLevel(Cassandra.ConsistencyLevel)"/> and
-        /// <see cref="SetGraphWriteConsistencyLevel(Cassandra.ConsistencyLevel)"/> will override the consistency set
-        /// here.
-        /// </para>
-        /// </summary>
-        public GraphStatement SetConsistencyLevel(ConsistencyLevel consistency)
+        /// <inheritdoc />
+        public IGraphStatement SetConsistencyLevel(ConsistencyLevel consistency)
         {
             ConsistencyLevel = consistency;
             return this;
         }
 
-        /// <summary>
-        /// Sets the graph language to use with this statement.
-        /// <para>
-        /// This property is not required; if it is not set, the default <see cref="GraphOptions.Language"/> will be
-        /// used when executing the statement.
-        /// </para>
-        /// </summary>
-        public GraphStatement SetGraphLanguage(string language)
+        /// <inheritdoc />
+        public IGraphStatement SetGraphLanguage(string language)
         {
             GraphLanguage = language;
             return this;
         }
 
-        /// <summary>
-        /// Sets the graph name to use in graph queries.
-        /// If you don't call this method, it is left unset.
-        /// </summary>
-        public GraphStatement SetGraphName(string name)
+        /// <inheritdoc />
+        public IGraphStatement SetGraphName(string name)
         {
             GraphName = name;
             return this;
         }
 
-        /// <summary>
-        /// Sets the consistency level used for the graph read query.
-        /// <para>
-        /// This setting will override the consistency level set with 
-        /// <see cref="SetConsistencyLevel(Cassandra.ConsistencyLevel)"/> only for the READ part of the graph query.
-        /// </para>
-        /// </summary>
-        public GraphStatement SetGraphReadConsistencyLevel(ConsistencyLevel consistency)
+        /// <inheritdoc />
+        public IGraphStatement SetGraphReadConsistencyLevel(ConsistencyLevel consistency)
         {
             GraphReadConsistencyLevel = consistency;
             return this;
         }
 
-        /// <summary>
-        /// Sets the graph traversal source name to use in graph queries.
-        /// If you don't call this method, it defaults to <see cref="GraphOptions.Source"/>.
-        /// </summary>
-        public GraphStatement SetGraphSource(string source)
+        /// <inheritdoc />
+        public IGraphStatement SetGraphSource(string source)
         {
             GraphSource = source;
             return this;
         }
 
-        /// <summary>
-        /// Sets the graph source to the server-defined analytic traversal source ("a") for this statement.
-        /// </summary>
-        public GraphStatement SetGraphSourceAnalytics()
+        /// <inheritdoc />
+        public IGraphStatement SetGraphSourceAnalytics()
         {
             return SetGraphSource("a");
         }
 
-        /// <summary>
-        /// Sets the consistency level used for the graph write query.
-        /// <para>
-        /// This setting will override the consistency level set with 
-        /// <see cref="SetConsistencyLevel(Cassandra.ConsistencyLevel)"/> only for the WRITE part of the graph query.
-        /// </para>
-        /// </summary>
-        public GraphStatement SetGraphWriteConsistencyLevel(ConsistencyLevel consistency)
+        /// <inheritdoc />
+        public IGraphStatement SetGraphWriteConsistencyLevel(ConsistencyLevel consistency)
         {
             GraphReadConsistencyLevel = consistency;
             return this;
         }
 
-        /// <summary>
-        /// Sets the per-host read timeout in milliseconds for this statement.
-        /// <para>Use <see cref="Timeout.Infinite"/> to disable timeouts for this Statement.</para>
-        /// <para>Use zero to use the default value specified in the <see cref="GraphOptions.ReadTimeoutMillis"/>.</para>
-        /// </summary>
-        /// <param name="timeout">Timeout in milliseconds.</param>
-        /// <returns>This instance</returns>
-        public GraphStatement SetReadTimeoutMillis(int timeout)
+        /// <inheritdoc />
+        public IGraphStatement SetReadTimeoutMillis(int timeout)
         {
             ReadTimeoutMillis = timeout;
             return this;
         }
 
-        /// <summary>
-        /// Forces this statement to use no graph name, even if a default graph name was defined 
-        /// with <see cref="GraphOptions.SetName(string)"/>.
-        /// <para>
-        /// If a graph name was previously defined on this statement, it will be reset.
-        /// </para>
-        /// </summary>
-        public GraphStatement SetSystemQuery()
+        /// <inheritdoc />
+        public IGraphStatement SetSystemQuery()
         {
             IsSystemQuery = true;
             GraphName = null;
             return this;
         }
 
-        /// <summary>
-        /// Sets the timestamp associated with this query.
-        /// </summary>
-        public GraphStatement SetTimestamp(DateTimeOffset timestamp)
+        /// <inheritdoc />
+        public IGraphStatement SetTimestamp(DateTimeOffset timestamp)
         {
             Timestamp = timestamp;
             return this;
--- src/Dse/Graph/IGraphStatement.cs
+++ src/Dse/Graph/IGraphStatement.cs
@@ -64,5 +64,88 @@ namespace Dse.Graph
         /// </summary>
         /// <remarks>Used by the DSE driver to translate between this statement and Core driver statement instances</remarks>
         IStatement ToIStatement(GraphOptions options);
+
+        /// <summary>
+        /// Sets the consistency level to use for this statement.
+        /// <para>
+        /// This setting will affect the general consistency when executing the gremlin query. However
+        /// executing a gremlin query on the server side is going to involve the execution of CQL queries to the 
+        /// persistence engine that is Cassandra. Those queries can be both reads and writes and both will have a
+        /// settable consistency level. Setting only this property will indicate to the server to use this consistency
+        /// level for both reads and writes in Cassandra. Read or write consistency level can be set separately with
+        /// respectively
+        /// <see cref="SetGraphReadConsistencyLevel(Cassandra.ConsistencyLevel)"/> and
+        /// <see cref="SetGraphWriteConsistencyLevel(Cassandra.ConsistencyLevel)"/> will override the consistency set
+        /// here.
+        /// </para>
+        /// </summary>
+        IGraphStatement SetConsistencyLevel(ConsistencyLevel consistency);
+
+        /// <summary>
+        /// Sets the graph language to use with this statement.
+        /// <para>
+        /// This property is not required; if it is not set, the default <see cref="GraphOptions.Language"/> will be
+        /// used when executing the statement.
+        /// </para>
+        /// </summary>
+        IGraphStatement SetGraphLanguage(string language);
+
+        /// <summary>
+        /// Sets the graph name to use in graph queries.
+        /// If you don't call this method, it is left unset.
+        /// </summary>
+        IGraphStatement SetGraphName(string name);
+
+        /// <summary>
+        /// Sets the consistency level used for the graph read query.
+        /// <para>
+        /// This setting will override the consistency level set with 
+        /// <see cref="SetConsistencyLevel(Cassandra.ConsistencyLevel)"/> only for the READ part of the graph query.
+        /// </para>
+        /// </summary>
+        IGraphStatement SetGraphReadConsistencyLevel(ConsistencyLevel consistency);
+
+        /// <summary>
+        /// Sets the graph traversal source name to use in graph queries.
+        /// If you don't call this method, it defaults to <see cref="GraphOptions.Source"/>.
+        /// </summary>
+        IGraphStatement SetGraphSource(string source);
+
+        /// <summary>
+        /// Sets the graph source to the server-defined analytic traversal source ("a") for this statement.
+        /// </summary>
+        IGraphStatement SetGraphSourceAnalytics();
+
+        /// <summary>
+        /// Sets the consistency level used for the graph write query.
+        /// <para>
+        /// This setting will override the consistency level set with 
+        /// <see cref="SetConsistencyLevel(Cassandra.ConsistencyLevel)"/> only for the WRITE part of the graph query.
+        /// </para>
+        /// </summary>
+        IGraphStatement SetGraphWriteConsistencyLevel(ConsistencyLevel consistency);
+
+        /// <summary>
+        /// Sets the per-host read timeout in milliseconds for this statement.
+        /// <para>Use <see cref="Timeout.Infinite"/> to disable timeouts for this Statement.</para>
+        /// <para>Use zero to use the default value specified in the <see cref="GraphOptions.ReadTimeoutMillis"/>.</para>
+        /// </summary>
+        /// <param name="timeout">Timeout in milliseconds.</param>
+        /// <returns>This instance</returns>
+        IGraphStatement SetReadTimeoutMillis(int timeout);
+
+        /// <summary>
+        /// Forces this statement to use no graph name, even if a default graph name was defined 
+        /// with <see cref="GraphOptions.SetName(string)"/>.
+        /// <para>
+        /// If a graph name was previously defined on this statement, it will be reset.
+        /// </para>
+        /// </summary>
+        IGraphStatement SetSystemQuery();
+
+        /// <summary>
+        /// Sets the timestamp associated with this query.
+        /// </summary>
+        IGraphStatement SetTimestamp(DateTimeOffset timestamp);
     }
 }
