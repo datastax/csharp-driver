10f235945e24d9b032b167a0a8d5cd6556e8decd
Peter Mate <peter.mate@logmein.com>
2016-09-01 09:48:12 +0200

    Add EC2MultiRegionAddressTranslator

---
+++ src/Cassandra.Tests/EC2MultiRegionAddressTranslatorTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cassandra.Tests
+{
+    [TestFixture]
+    public class EC2MultiRegionAddressTranslatorTest
+    {
+        public EC2MultiRegionAddressTranslatorTest()
+        {
+            Diagnostics.CassandraTraceSwitch.Level = System.Diagnostics.TraceLevel.Info;
+        }
+
+        [Test]
+        public void ShouldReturnSameAddressWhenNoEntryFound()
+        {
+            Mock<IDnsWrapper> dnsMock = new Mock<IDnsWrapper>();
+            //System.Net.Dns throws SocketException if no hostname is found for address
+            dnsMock.Setup(d => d.GetHostEntry(It.IsAny<IPAddress>())).Throws(new SocketException());
+            EC2MultiRegionAddressTranslator translator = new EC2MultiRegionAddressTranslator(dnsMock.Object);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("10.0.0.2"), 9042);
+            Assert.AreEqual(translator.Translate(endpoint), endpoint);
+        }
+
+        [Test]
+        public void ShouldReturnSameAddressWhenExceptionEncountered()
+        {
+            Mock<IDnsWrapper> dnsMock = new Mock<IDnsWrapper>();
+            dnsMock.Setup(d => d.GetHostEntry(It.IsAny<IPAddress>())).Throws(new Exception());
+            EC2MultiRegionAddressTranslator translator = new EC2MultiRegionAddressTranslator(dnsMock.Object);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("10.0.0.2"), 9042);
+            Assert.AreEqual(translator.Translate(endpoint), endpoint);
+        }
+
+        [Test]
+        public void ShouldReturnNewAddressWhenMatchFound()
+        {
+            IPEndPoint expectedEndpoint = new IPEndPoint(IPAddress.Parse("10.0.0.2"), 9042);
+
+            Mock<IDnsWrapper> dnsMock = new Mock<IDnsWrapper>();
+            dnsMock.Setup(d => d.GetHostEntry(It.IsAny<IPAddress>()))
+                .Returns(() => new IPHostEntry() { HostName = "ec2-publicaddress.amazonaws.com" });
+            dnsMock.Setup(d => d.GetHostEntry(It.IsAny<string>()))
+                .Returns(() => new IPHostEntry()
+                {
+                    HostName = "ec2-publicaddress.amazonaws.com",
+                    AddressList = new IPAddress[] { IPAddress.Parse("10.0.0.2") }
+                });
+            EC2MultiRegionAddressTranslator translator = new EC2MultiRegionAddressTranslator(dnsMock.Object);
+            IPEndPoint publicEndpoint = new IPEndPoint(IPAddress.Parse("56.12.34.56"), 9042);
+            Assert.AreEqual(translator.Translate(publicEndpoint), expectedEndpoint);
+        }
+    }
+}
---
+++ src/Cassandra/EC2MultiRegionAddressTranslator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cassandra
+{
+    public interface IDnsWrapper
+    {
+        IPHostEntry GetHostEntry(string hostNameOrAddress);
+        IPHostEntry GetHostEntry(IPAddress address);
+    }
+
+    public class DnsWrapper : IDnsWrapper
+    {
+        public IPHostEntry GetHostEntry(string hostNameOrAddress)
+        {
+            return Dns.GetHostEntry(hostNameOrAddress);
+        }
+
+        public IPHostEntry GetHostEntry(IPAddress address)
+        {
+            return Dns.GetHostEntry(address);
+        }
+    }
+
+    public class EC2MultiRegionAddressTranslator : IAddressTranslator
+    {
+        private static readonly Logger _logger = new Logger(typeof(EC2MultiRegionAddressTranslator));
+
+        private readonly IDnsWrapper _dns;
+
+        public EC2MultiRegionAddressTranslator()
+        {
+            _dns = new DnsWrapper();
+        }
+
+        public EC2MultiRegionAddressTranslator(IDnsWrapper dnsWrapper)
+        {
+            _dns = dnsWrapper;
+        }
+
+        public IPEndPoint Translate(IPEndPoint address)
+        {
+            try
+            {
+                string DomainName = _dns.GetHostEntry(address.Address).HostName;
+                IPAddress translatedAddress = _dns.GetHostEntry(DomainName).AddressList.First();
+                _logger.Verbose("Resolved {0} to {1}", address.Address, translatedAddress);
+                return new IPEndPoint(translatedAddress, address.Port);
+            }
+            catch (SocketException)
+            {
+                _logger.Warning("Domain name not found for {0}, returning it as-is.", address.Address);
+                return address;
+            }
+            catch (Exception ex)
+            {
+                _logger.Warning("Error translating {0}, returning it as-is. Exception: {1}", address.Address, ex.Message);
+                return address;
+            }
+        }
+    }
+}
