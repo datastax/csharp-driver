c0e33b4c0d6e2d1f5dc99fa1ecc48133f862065e
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2016-11-21 16:20:57 +0100

    Remove unused `using` directives

--- src/Cassandra.Tests/LoggingTests.cs
+++ src/Cassandra.Tests/LoggingTests.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 
 namespace Cassandra.Tests
