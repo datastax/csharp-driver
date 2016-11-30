c02b521c5f7a02ca2d68f93b4a0168c825080283
Jorge Bay Gondra <jorge.gondra@datastax.com>
2016-11-30 11:56:41 +0100

    Alternate ILoggerProvider-based logging API	 (#269)

--- doc/faq/README.md
+++ doc/faq/README.md
@@ -5,7 +5,24 @@
 Normally you should use one `ISession` instance per application. You should share that instance between classes within
 your application.
 
-## How can I enable tracing in the driver?
+## How can I enable logging in the driver?
+
+The driver allows you to plug in any [`ILoggerProvider`][logging-api] implementation, like [NLog][nlog] and
+[Serilog][serilog] implementations.
+
+You should set the provider before initializing the cluster, using the `Diagnostics` class:
+
+```csharp
+// Use the provider you prefer, in this case NLog
+ILoggerProvider provider = new NLogLoggerProvider();
+// Add it before initializing the Cluster
+Cassandra.Diagnostics.AddLoggerProvider(provider);
+```
+
+You can configure the log levels you want to output using the provider API.
+
+Alternatively, if you don't want to use a `ILoggerProvider` implementation, the driver can expose log events using
+the .NET Tracing API.
 
 ```csharp
 // Specify the minimum trace level you want to see
@@ -18,7 +35,7 @@ Trace.Listeners.Add(new ConsoleTraceListener());
 
 It depends on the size of the requests and the number of tables affected by the BATCH. Large batches can
 cause a lot of stress on the coordinator. Consider that Cassandra batches are not suitable for bulk loading, there
-are dedicated tools for that. batches allow you to group related updates in a single request, so keep the BATCH size
+are dedicated tools for that. Batches allow you to group related updates in a single request, so keep the BATCH size
 small (in the order of tens).
 
 Starting from Cassandra version 2.0.8, the node issues a warning if the batch size is greater than 5K.
@@ -27,3 +44,7 @@ Starting from Cassandra version 2.0.8, the node issues a warning if the batch si
 
 You can decrease the number of rows retrieved per page. By using the `SetPageSize()` method on a statement, you
 instruct the driver to retrieve fewer rows per request (the default is 5000).
+
+[logging-api]: https://github.com/aspnet/Logging
+[nlog]: https://github.com/NLog/NLog.Extensions.Logging
+[serilog]: https://github.com/serilog/serilog-extensions-logging
---
+++ src/Cassandra.Tests/LoggingTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Cassandra.Tests
+{
+    [TestFixture]
+    public class LoggingTests
+    {
+        [Test]
+        public void FactoryBasedLoggerHandler_Methods_Not_Throw()
+        {
+            UseAllMethods(new Logger.FactoryBasedLoggerHandler(typeof(int)));
+        }
+
+        [Test]
+        public void FactoryBasedLoggerHandler_Methods_Should_Output_To_Trace()
+        {
+            var originalLevel = Diagnostics.CassandraTraceSwitch.Level;
+            Diagnostics.CassandraTraceSwitch.Level = TraceLevel.Verbose;
+            var listener = new TestTraceListener();
+            Trace.Listeners.Add(listener);
+            UseAllMethods(new Logger.TraceBasedLoggerHandler(typeof(int)));
+            Trace.Listeners.Remove(listener);
+            Assert.AreEqual(6, listener.Messages.Count);
+            Diagnostics.CassandraTraceSwitch.Level = originalLevel;
+            var expectedMessages = new[]
+            {
+                "Test exception 1",
+                "Message 1",
+                "Message 2 Param1",
+                "Message 3 Param2",
+                "Message 4 Param3",
+                "Message 5 Param4"
+            };
+            var messages = listener.Messages.Keys.OrderBy(k => k).Select(k => listener.Messages[k]).ToArray();
+            for (var i = 0; i < expectedMessages.Length; i++)
+            {
+                StringAssert.Contains(expectedMessages[i], messages[i]);
+            }
+        }
+
+        private void UseAllMethods(Logger.ILoggerHandler loggerHandler)
+        {
+            loggerHandler.Error(new Exception("Test exception 1"));
+            loggerHandler.Error("Message 1", new Exception("Test exception 1"));
+            loggerHandler.Error("Message 2 {0}", "Param1");
+            loggerHandler.Info("Message 3 {0}", "Param2");
+            loggerHandler.Verbose("Message 4 {0}", "Param3");
+            loggerHandler.Warning("Message 5 {0}", "Param4");
+        }
+
+        private class TestTraceListener : TraceListener
+        {
+            public readonly ConcurrentDictionary<int, string> Messages = new ConcurrentDictionary<int, string>();
+            private int _counter = -1;
+
+            public override void Write(string message)
+            {
+            }
+
+            public override void WriteLine(string message)
+            {
+                Messages.AddOrUpdate(Interlocked.Increment(ref _counter), message, (k, v) => message);
+            }
+        }
+    }
+}
