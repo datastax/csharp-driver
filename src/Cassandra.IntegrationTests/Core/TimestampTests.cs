ae173984f09905a3c7deb1f6de7ad0a0289ab8b7
Arthur Landim <landim.arthur@gmail.com>
2017-03-21 11:01:50 -0300

    Remove timestamp test: USING TIMESTAMP

--- src/Cassandra.IntegrationTests/Core/TimestampTests.cs
+++ src/Cassandra.IntegrationTests/Core/TimestampTests.cs
@@ -59,41 +59,6 @@ namespace Cassandra.IntegrationTests.Core
             }
         }
 
-        [Test]
-        public void Should_Use_CQL_Timestamp_Precedence_Over_Cluster_Timestamp_Generator()
-        {
-            var generator = new TestTimestampGenerator();
-            using (var cluster = Cluster.Builder()
-                                        .WithTimestampGenerator(generator)
-                                        .AddContactPoint(TestCluster.InitialContactPoint)
-                                        .Build())
-            {
-                var session = cluster.Connect();
-                session.CreateKeyspace("timestamp_tests",null, false);
-                session.ChangeKeyspace("timestamp_tests");
-                QueryTools.ExecuteSyncNonQuery(session, string.Format(@"
-                CREATE TABLE {0}(
-                id uuid PRIMARY KEY,
-                label text);", "tbl_timestamp_test"));
-                var now = DateTimeOffset.Parse("2017-01-01T00:00:00.000+00:00");
-                var id = Guid.NewGuid();
-                var stmt = new SimpleStatement("INSERT INTO tbl_timestamp_test (id, label) VALUES (?,?) USING TIMESTAMP ?",
-                    id, "test label", now.Ticks);
-                session.Execute(stmt);
-                // The driver should use the generator against C* 2.1+
-                Assert.AreEqual(CassandraVersion < new Version(2, 1) ? 0 : 4, generator.GetCounter());
-
-                //verify if the write time was according to USING TIMESTAMP now timestamp generator
-                var writetimeStmt = new SimpleStatement("select writetime(label) as insert_timestamp from tbl_timestamp_test where id = ?",
-                    id);
-                var rs = session.Execute(writetimeStmt);
-                Assert.NotNull(rs);
-                var first = rs.FirstOrDefault();
-                Assert.NotNull(first);
-                Assert.AreEqual(now.Ticks, first.GetValue<Int64>("insert_timestamp"));
-            }
-        }
-
         private class TestTimestampGenerator : ITimestampGenerator
         {
             private int _counter;
