60961c3effd41b531e316a18192f5d4225039834
Arthur Landim <arthur.landim@datastax.com>
2017-07-11 14:27:52 -0300

    Test: Set batch type for Linq methods

--- src/Cassandra.IntegrationTests/Linq/LinqMethods/Counter.cs
+++ src/Cassandra.IntegrationTests/Linq/LinqMethods/Counter.cs
@@ -80,59 +80,6 @@ namespace Cassandra.IntegrationTests.Linq.LinqMethods
             }
         }
 
-
-        /// <summary>
-        /// Do many counter updates in parallel
-        /// </summary>
-        [Test, Category("long")]
-        public void LinqAttributes_Counter_Parallel()
-        {
-            //var mapping = new Map<PocoWithCounter>();
-            var mappingConfig = new MappingConfiguration();
-            mappingConfig.MapperFactory.PocoDataFactory.AddDefinitionDefault(typeof(CounterEntityWithLinqAttributes),
-                 () => LinqAttributeBasedTypeDefinition.DetermineAttributes(typeof(CounterEntityWithLinqAttributes)));
-            var table = new Table<CounterEntityWithLinqAttributes>(_session, mappingConfig);
-            table.Create();
-
-            List<CounterEntityWithLinqAttributes> counterPocos = new List<CounterEntityWithLinqAttributes>();
-            for (int i = 0; i < 100; i++)
-            {
-                counterPocos.Add(
-                    new CounterEntityWithLinqAttributes()
-                    {
-                        KeyPart1 = Guid.NewGuid(),
-                        KeyPart2 = (decimal)123,
-                    });
-            }
-
-            int counterIncrements = 2000;
-            string updateStr = String.Format("UPDATE \"{0}\" SET \"{1}\"=\"{1}\" + 1 WHERE \"{2}\"=? and \"{3}\"=?", typeof(CounterEntityWithLinqAttributes).Name, "Counter", "KeyPart1", "KeyPart2");
-            var updateSession = _session.Prepare(updateStr);
-            Parallel.ForEach(counterPocos, pocoWithCounter =>
-            {
-                var boundStatement = updateSession.Bind(new object[] {pocoWithCounter.KeyPart1, pocoWithCounter.KeyPart2});
-                for (int j = 0; j < counterIncrements; j++)
-                    _session.Execute(boundStatement);
-                pocoWithCounter.Counter += counterIncrements;
-            });
-
-            List<CounterEntityWithLinqAttributes> countersQueried = table.Select(m => m).Execute().ToList();
-            foreach (CounterEntityWithLinqAttributes pocoWithCounterExpected in counterPocos)
-            {
-                bool counterFound = false;
-                foreach (CounterEntityWithLinqAttributes pocoWithCounterActual in countersQueried)
-                {
-                    if (pocoWithCounterExpected.KeyPart1 == pocoWithCounterActual.KeyPart1)
-                    {
-                        Assert.AreEqual(pocoWithCounterExpected.KeyPart2, pocoWithCounterExpected.KeyPart2);
-                        Assert.AreEqual(pocoWithCounterExpected.Counter, pocoWithCounterExpected.Counter);
-                        counterFound = true;
-                    }
-                }
-                Assert.IsTrue(counterFound, "Counter with first key part: " + pocoWithCounterExpected.KeyPart1 + " was not found!");
-            }
-        }
-
         /// <summary>
         /// Validate expected error message when attempting to insert a row that contains a counter
         /// </summary>
@@ -233,6 +180,45 @@ namespace Cassandra.IntegrationTests.Linq.LinqMethods
             Assert.AreEqual(0, updatedCounter.Counter);
         }
 
+        [Test]
+        public void LinqCounter_BatchTest()
+        {
+            var mappingConfig = new MappingConfiguration();
+            mappingConfig.Define(new Map<CounterEntityWithLinqAttributes>()
+                .ExplicitColumns()
+                .Column(t => t.KeyPart1)
+                .Column(t => t.KeyPart2)
+                .Column(t => t.Counter, map => map.AsCounter())
+                .PartitionKey(t => t.KeyPart1, t => t.KeyPart2)
+                .TableName("linqcounter_batchtest_table")
+            );
+            var counterTable = new Table<CounterEntityWithLinqAttributes>(_session, mappingConfig);
+            counterTable.CreateIfNotExists();
+            var counter = new CounterEntityWithLinqAttributes { KeyPart1 = Guid.NewGuid(), KeyPart2 = 1, Counter = 1 };
+            var counter2 = new CounterEntityWithLinqAttributes { KeyPart1 = counter.KeyPart1, KeyPart2 = 2, Counter = 2 };
+
+            var batch = counterTable.GetSession().CreateBatch(BatchType.Counter);
+
+            var update1 = counterTable
+                .Where(t => t.KeyPart1 == counter.KeyPart1 && t.KeyPart2 == counter.KeyPart2)
+                .Select(t => new CounterEntityWithLinqAttributes { Counter = 1 })
+                .Update();
+
+            var update2 = counterTable
+                .Where(t => t.KeyPart1 == counter2.KeyPart1 && t.KeyPart2 == counter2.KeyPart2)
+                .Select(t => new CounterEntityWithLinqAttributes { Counter = 2 })
+                .Update();
+
+            batch.Append(update1);
+            batch.Append(update2);
+
+            batch.Execute();
+
+            var counters = counterTable.Execute().ToList();
+            Assert.AreEqual(2, counters.Count);
+            Assert.IsTrue(counters.Contains(counter));
+            Assert.IsTrue(counters.Contains(counter2));
+        }
 
         [Cassandra.Data.Linq.Table]
         class CounterEntityWithLinqAttributes
@@ -243,6 +229,28 @@ namespace Cassandra.IntegrationTests.Linq.LinqMethods
             public Guid KeyPart1;
             [Cassandra.Data.Linq.PartitionKey(2)]
             public Decimal KeyPart2;
+
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null)
+                {
+                    return false;
+                }
+                var comp = (CounterEntityWithLinqAttributes)obj;
+
+                return (this.Counter == comp.Counter
+                        && this.KeyPart1.Equals(comp.KeyPart1)
+                        && this.KeyPart2.Equals(comp.KeyPart2));
+            }
+
+            public override int GetHashCode()
+            {
+                var hash = KeyPart1.GetHashCode();
+                hash += KeyPart2.GetHashCode() * 1000;
+                hash += (int)Counter * 100000;
+                return hash;
+            }
         }
 
 
