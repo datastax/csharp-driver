feb63d0c095d4aa0ba317bb00eaa3971e9f54c75
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2017-09-29 13:45:58 +0200

    Change the message for batch log write failures
    Use a differentmessage for batch log write failures.
    Also fixed WriteFailureException message.

--- src/Cassandra/Exceptions/WriteTimeoutException.cs
+++ src/Cassandra/Exceptions/WriteTimeoutException.cs
@@ -17,23 +17,41 @@
 namespace Cassandra
 {
     /// <summary>
-    ///  A Cassandra timeout during a write query.
+    /// Represents a server timeout during a write operation.
     /// </summary>
     public class WriteTimeoutException : QueryTimeoutException
     {
-        public string WriteType { get; private set; }
+        private const string BatchLogMessage = "Server timeout during batchlog write at consistency {0}" +
+            " ({1} peer(s) acknowledged the write over {2} required)";
+        
+        private const string QueryMessage = "Server timeout during write query at consistency {0}" +
+            " ({1} peer(s) acknowledged the write over {2} required)";
+        
+        private const string BatchLogWriteType = "BATCH_LOG";
+        
+        /// <summary>
+        /// Gets the type of write operation that timed out.
+        /// <para>Possible values: SIMPLE, BATCH, BATCH_LOG, UNLOGGED_BATCH and COUNTER.</para>
+        /// </summary>
+        public string WriteType { get; }
 
+        /// <summary>
+        /// Creates a new instance of <see cref="WriteTimeoutException"/>
+        /// </summary>
         public WriteTimeoutException(ConsistencyLevel consistency, int received, int required,
-                                     string writeType) :
-                                         base(
-                                         string.Format(
-                                             "Cassandra timeout during write query at consistency {0} ({1} replica(s) acknowledged the write over {2} required)",
-                                             consistency.ToString().ToUpper(), received, required),
+                                     string writeType) : base(
+                                         GetMessage(writeType, consistency, received, required),
                                          consistency,
                                          received,
                                          required)
         {
             WriteType = writeType;
+        }
+
+        private static string GetMessage(string writeType, ConsistencyLevel consistency, int received, int required)
+        {
+            var message = writeType == BatchLogWriteType ? BatchLogMessage : QueryMessage;
+            return string.Format(message, consistency.ToString().ToUpper(), received, required);
         }
     }
 }
