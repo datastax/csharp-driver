84e4ed0ec4e4654d9d626a180eda18933e0648c8
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-05-31 16:17:36 +0200

    Log driver version on Connect

--- src/Cassandra/Cluster.cs
+++ src/Cassandra/Cluster.cs
@@ -17,8 +17,10 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Cassandra.Collections;
@@ -230,6 +232,7 @@ namespace Cassandra
                     //There was an exception that is not possible to recover from
                     throw _initException;
                 }
+                _logger.Info("Connecting to cluster using {0}", GetAssemblyInfo());
                 try
                 {
                     // Only abort the async operations when at least twice the time for ConnectTimeout per host passed
@@ -274,6 +277,13 @@ namespace Cassandra
             {
                 _initLock.Release();
             }
+        }
+
+        private static string GetAssemblyInfo()
+        {
+            var assembly = typeof(ISession).GetTypeInfo().Assembly;
+            var info = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return $"{info.ProductName} v{info.FileVersion}";
         }
 
         /// <inheritdoc />
