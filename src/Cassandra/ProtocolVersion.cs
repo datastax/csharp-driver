9798bf7c61aa659700116f30f279a404a077fe3b
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-06-07 13:18:40 +0200

    Use host metadata in protocol negotiation

--- src/Cassandra/ProtocolVersion.cs
+++ src/Cassandra/ProtocolVersion.cs
@@ -13,6 +13,9 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+using System;
+using System.Collections.Generic;
+
 namespace Cassandra
 {
     /// <summary>
@@ -60,6 +63,12 @@ namespace Cassandra
 
     internal static class ProtocolVersionExtensions
     {
+        private static readonly Logger Logger = new Logger(typeof(ProtocolVersion));
+        private static readonly Version Version30 = new Version(3, 0);
+        private static readonly Version Version22 = new Version(2, 2);
+        private static readonly Version Version21 = new Version(2, 1);
+        private static readonly Version Version20 = new Version(2, 0);
+
         /// <summary>
         /// Determines if the protocol version is supported by this driver.
         /// </summary>
@@ -85,6 +94,55 @@ namespace Cassandra
                 return 0;
             }
             return version - 1;
+        }
+
+        public static ProtocolVersion GetHighestCommon(this ProtocolVersion version, IEnumerable<Host> hosts)
+        {
+            var maxVersion = (byte)version;
+            var v3Requirement = false;
+            var maxVersionWith3OrMore = maxVersion;
+
+            foreach (var host in hosts)
+            {
+                var cassandraVersion = host.CassandraVersion;
+                if (cassandraVersion >= Version30)
+                {
+                    // Anything 3.0.0+ has a max protocol version of V4 and requires at least V3.
+                    v3Requirement = true;
+                    maxVersion = Math.Min((byte)ProtocolVersion.V4, maxVersion);
+                    maxVersionWith3OrMore = maxVersion;
+                }
+                else if (cassandraVersion >= Version22)
+                {
+                    // Cassandra 2.2.x has a max protocol version of V4.
+                    maxVersion = Math.Min((byte)ProtocolVersion.V4, maxVersion);
+                    maxVersionWith3OrMore = maxVersion;
+                }
+                else if (cassandraVersion >= Version21)
+                {
+                    // Cassandra 2.1.x has a max protocol version of V3.
+                    maxVersion = Math.Min((byte)ProtocolVersion.V3, maxVersion);
+                    maxVersionWith3OrMore = maxVersion;
+                }
+                else if (cassandraVersion >= Version20)
+                {
+                    // Cassandra 2.0.x has a max protocol version of V2.
+                    maxVersion = Math.Min((byte)ProtocolVersion.V2, maxVersion);
+                }
+                else
+                {
+                    // Anything else is < 2.x and requires protocol version V1.
+                    maxVersion = Math.Min((byte)ProtocolVersion.V1, maxVersion);
+                }
+            }
+
+            if (v3Requirement && maxVersion < (byte)ProtocolVersion.V3)
+            {
+                Logger.Error($"Detected hosts with maximum protocol version of {maxVersion} but there are some hosts " +
+                             $"that require at least version 3. Will not be able to connect to these older hosts");
+                maxVersion = maxVersionWith3OrMore;
+            }
+            return (ProtocolVersion)maxVersion;
         }
 
         /// <summary>
