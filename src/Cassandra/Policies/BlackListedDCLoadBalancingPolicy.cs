4dee152d16d563871c9083509827ba724de17c84
slapenko <serhiy.lapenko@gmail.com>
2018-08-13 20:56:42 -0500

    Added black listed data centers policy

---
+++ src/Cassandra/Policies/BlackListedDCLoadBalancingPolicy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Cassandra
+{
+    public class BlackListedDCLoadBalancingPolicy : ILoadBalancingPolicy
+    {
+        private ILoadBalancingPolicy _childPolicy;
+
+        private readonly IList<string> blacklistedDC;
+
+        public BlackListedDCLoadBalancingPolicy(IList<string> blacklistedDC)
+            : this(blacklistedDC, Policies.DefaultLoadBalancingPolicy)
+        {
+        }
+
+        public BlackListedDCLoadBalancingPolicy(IList<string> blacklistedDC, ILoadBalancingPolicy childPolicy)
+        {
+            _childPolicy = childPolicy;
+            this.blacklistedDC = blacklistedDC;
+        }
+
+        public HostDistance Distance(Host host)
+        {
+            if (blacklistedDC?.Any(x => x?.ToLower() == host.Datacenter.ToLower()) ?? false)
+                return HostDistance.Ignored;
+            return _childPolicy.Distance(host); ;
+        }
+
+        public void Initialize(ICluster cluster)
+        {
+            _childPolicy.Initialize(cluster);
+        }
+
+        public IEnumerable<Host> NewQueryPlan(string keyspace, IStatement query)
+        {
+            return _childPolicy.NewQueryPlan(keyspace, query);
+        }
+    }
+}
