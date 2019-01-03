55ce9db0bafd5e7cfb06db155ba482bd87d42e39
João Reis <joao.r.reis@outlook.com>
2019-01-03 18:07:18 +0000

    added integration tests for current host retry policy

--- src/Cassandra.IntegrationTests/Policies/Tests/RetryPolicyShortTests.cs
+++ src/Cassandra.IntegrationTests/Policies/Tests/RetryPolicyShortTests.cs
@@ -16,10 +16,11 @@
 
 using System;
 using System.Threading;
+
 using Cassandra.IntegrationTests.TestBase;
 using Cassandra.IntegrationTests.TestClusterManagement;
 using Cassandra.IntegrationTests.TestClusterManagement.Simulacron;
-using Newtonsoft.Json.Linq;
+
 using NUnit.Framework;
 
 namespace Cassandra.IntegrationTests.Policies.Tests
@@ -36,126 +37,205 @@ namespace Cassandra.IntegrationTests.Policies.Tests
         {
             TestClusterManager.TryRemove();
         }
-        
+
         [TestCase("overloaded", typeof(OverloadedException))]
         [TestCase("is_bootstrapping", typeof(IsBootstrappingException))]
         public void RetryPolicy_Extended(string resultError, Type exceptionType)
         {
-            var simulacronCluster = SimulacronCluster.CreateNew(new SimulacronOptions());
-            var contactPoint = simulacronCluster.InitialContactPoint;
-            var extendedRetryPolicy = new TestExtendedRetryPolicy();
-            var builder = Cluster.Builder()
-                                 .AddContactPoint(contactPoint)
-                                 .WithRetryPolicy(extendedRetryPolicy)
-                                 .WithReconnectionPolicy(new ConstantReconnectionPolicy(long.MaxValue));
-            using (var cluster = builder.Build())
+            using (var simulacronCluster = SimulacronCluster.CreateNew(new SimulacronOptions()))
             {
-                var session = (Session) cluster.Connect();
-                const string cql = "select * from table1";
-                
-                var primeQuery = new
+                var contactPoint = simulacronCluster.InitialContactPoint;
+                var extendedRetryPolicy = new TestExtendedRetryPolicy();
+                var builder = Cluster.Builder()
+                                     .AddContactPoint(contactPoint)
+                                     .WithRetryPolicy(extendedRetryPolicy)
+                                     .WithReconnectionPolicy(new ConstantReconnectionPolicy(long.MaxValue));
+                using (var cluster = builder.Build())
                 {
-                    when = new { query = cql },
-                    then = new
+                    var session = (Session)cluster.Connect();
+                    const string cql = "select * from table1";
+
+                    var primeQuery = new
                     {
-                        result = resultError, 
-                        delay_in_ms = 0,
-                        message = resultError,
-                        ignore_on_prepare = false
+                        when = new { query = cql },
+                        then = new
+                        {
+                            result = resultError,
+                            delay_in_ms = 0,
+                            message = resultError,
+                            ignore_on_prepare = false
+                        }
+                    };
+
+                    simulacronCluster.Prime(primeQuery);
+                    Exception throwedException = null;
+                    try
+                    {
+                        session.Execute(cql);
                     }
-                };
-                
-                simulacronCluster.Prime(primeQuery);
-                Exception throwedException = null;
-                try
-                {
-                    session.Execute(cql);
-                }
-                catch (Exception ex)
-                {
-                    throwedException = ex;
-                }
-                finally
-                {
-                    Assert.NotNull(throwedException);
-                    Assert.AreEqual(throwedException.GetType(), exceptionType);
-                    Assert.AreEqual(1, Interlocked.Read(ref extendedRetryPolicy.RequestErrorConter));
-                    Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.ReadTimeoutCounter));
-                    Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.WriteTimeoutCounter));
-                    Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.UnavailableCounter));
+                    catch (Exception ex)
+                    {
+                        throwedException = ex;
+                    }
+                    finally
+                    {
+                        Assert.NotNull(throwedException);
+                        Assert.AreEqual(throwedException.GetType(), exceptionType);
+                        Assert.AreEqual(1, Interlocked.Read(ref extendedRetryPolicy.RequestErrorConter));
+                        Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.ReadTimeoutCounter));
+                        Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.WriteTimeoutCounter));
+                        Assert.AreEqual(0, Interlocked.Read(ref extendedRetryPolicy.UnavailableCounter));
+                    }
                 }
             }
         }
-        
+
         [TestCase(true)]
         [TestCase(false)]
         [Test]
-        public async Task teste(bool async)
+        public async Task Should_RetryOnNextHost_When_SendFailsOnCurrentHostRetryPolicy(bool async)
         {
-            var simulacronCluster = SimulacronCluster.CreateNew(3);
-            var contactPoint = simulacronCluster.InitialContactPoint;
-            var nodes = simulacronCluster.GetNodes().ToArray();
-            var currentHostRetryPolicy = new CurrentHostRetryPolicy();
-            var loadBalancingPolicy = new CustomLoadBalancingPolicy(
-                nodes.Select(n => n.ContactPoint).ToArray());
-            var builder = Cluster.Builder()
-                                 .AddContactPoint(contactPoint)
-                                 .WithSocketOptions(new SocketOptions()
-                                                    .SetConnectTimeoutMillis(10000)
-                                                    .SetReadTimeoutMillis(5000))
-                                 .WithLoadBalancingPolicy(loadBalancingPolicy)
-                                 .WithRetryPolicy(currentHostRetryPolicy);
-            using (var cluster = builder.Build())
+            using (var simulacronCluster = SimulacronCluster.CreateNew(3))
             {
-                var session = (Session) cluster.Connect();
-                const string cql = "select * from table2";
-                
-                var primeQueryFirstNode = new
+                var contactPoint = simulacronCluster.InitialContactPoint;
+                var nodes = simulacronCluster.GetNodes().ToArray();
+                var queryPlan = new List<SimulacronNode>
                 {
-                    when = new { query = cql },
-                    then = new
-                    {
-                        result = "overloaded", 
-                        delay_in_ms = 0,
-                        message = "overloaded",
-                        ignore_on_prepare = false
-                    }
+                    nodes[1],
+                    nodes[2],
+                    nodes[0]
                 };
-
-                var primeQuerySecondNode = new
+                var currentHostRetryPolicy = new CurrentHostRetryPolicy(10, i => queryPlan[0].Stop());
+                var loadBalancingPolicy = new CustomLoadBalancingPolicy(
+                    queryPlan.Select(n => n.ContactPoint).ToArray());
+                var builder = Cluster.Builder()
+                                     .AddContactPoint(contactPoint)
+                                     .WithSocketOptions(new SocketOptions()
+                                                        .SetConnectTimeoutMillis(10000)
+                                                        .SetReadTimeoutMillis(5000))
+                                     .WithLoadBalancingPolicy(loadBalancingPolicy)
+                                     .WithRetryPolicy(currentHostRetryPolicy);
+                using (var cluster = builder.Build())
                 {
-                    when = new { query = cql },
-                    then = new
+                    var session = (Session)cluster.Connect();
+                    const string cql = "select * from table2";
+
+                    var primeQueryFirstNode = new
                     {
-                        result = "success", 
-                        delay_in_ms = 0,
-                        rows = new [] { "test1", "test2" }.Select(v => new { text = v }).ToArray(),
-                        column_types = new { text = "ascii" },
-                        ignore_on_prepare = false
+                        when = new { query = cql },
+                        then = new
+                        {
+                            result = "overloaded",
+                            delay_in_ms = 0,
+                            message = "overloaded",
+                            ignore_on_prepare = false
+                        }
+                    };
+
+                    var primeQuerySecondNode = new
+                    {
+                        when = new { query = cql },
+                        then = new
+                        {
+                            result = "success",
+                            delay_in_ms = 0,
+                            rows = new[] { "test1", "test2" }.Select(v => new { text = v }).ToArray(),
+                            column_types = new { text = "ascii" },
+                            ignore_on_prepare = false
+                        }
+                    };
+
+                    queryPlan[0].Prime(primeQueryFirstNode);
+                    queryPlan[1].Prime(primeQuerySecondNode);
+
+                    if (async)
+                    {
+                        await session.ExecuteAsync(
+                            new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One)).ConfigureAwait(false);
                     }
-                };
+                    else
+                    {
+                        session.Execute(new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One));
+                    }
 
-                nodes[0].Prime(primeQueryFirstNode);
-                nodes[1].Prime(primeQuerySecondNode);
-                if (async)
-                {
-                    await session.ExecuteAsync(
-                        new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One)).ConfigureAwait(false);
+                    Assert.AreEqual(1, currentHostRetryPolicy.RequestErrorCounter);
+                    Assert.AreEqual(1, (await queryPlan[0].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
+                    Assert.AreEqual(1, (await queryPlan[1].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
+                    Assert.AreEqual(0, (await queryPlan[2].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
                 }
-                else
-                {
-                    session.Execute(new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One));
-                }
-
-                Assert.AreEqual(2, currentHostRetryPolicy.RequestErrorConter);
-                Assert.AreEqual(new List<int> { 1, 2 }, currentHostRetryPolicy.Retries);
-                Assert.AreEqual(2, (await nodes[0].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
-                Assert.AreEqual(1, (await nodes[1].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
-                Assert.AreEqual(0, (await nodes[2].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
             }
         }
 
-        class TestExtendedRetryPolicy : IExtendedRetryPolicy
+        [TestCase(true)]
+        [TestCase(false)]
+        [Test]
+        public async Task Should_KeepRetryingOnSameHost_When_CurrentHostRetryPolicyIsSetAndSendSucceeds(bool async)
+        {
+            using (var simulacronCluster = SimulacronCluster.CreateNew(3))
+            {
+                var contactPoint = simulacronCluster.InitialContactPoint;
+                var nodes = simulacronCluster.GetNodes().ToArray();
+                var currentHostRetryPolicy = new CurrentHostRetryPolicy(10, null);
+                var loadBalancingPolicy = new CustomLoadBalancingPolicy(
+                    nodes.Select(n => n.ContactPoint).ToArray());
+                var builder = Cluster.Builder()
+                                     .AddContactPoint(contactPoint)
+                                     .WithSocketOptions(new SocketOptions()
+                                                        .SetConnectTimeoutMillis(10000)
+                                                        .SetReadTimeoutMillis(5000))
+                                     .WithLoadBalancingPolicy(loadBalancingPolicy)
+                                     .WithRetryPolicy(currentHostRetryPolicy);
+                using (var cluster = builder.Build())
+                {
+                    var session = (Session)cluster.Connect();
+                    const string cql = "select * from table2";
+
+                    var primeQueryFirstNode = new
+                    {
+                        when = new { query = cql },
+                        then = new
+                        {
+                            result = "overloaded",
+                            delay_in_ms = 0,
+                            message = "overloaded",
+                            ignore_on_prepare = false
+                        }
+                    };
+
+                    var primeQuerySecondNode = new
+                    {
+                        when = new { query = cql },
+                        then = new
+                        {
+                            result = "success",
+                            delay_in_ms = 0,
+                            rows = new[] { "test1", "test2" }.Select(v => new { text = v }).ToArray(),
+                            column_types = new { text = "ascii" },
+                            ignore_on_prepare = false
+                        }
+                    };
+
+                    nodes[0].Prime(primeQueryFirstNode);
+                    nodes[1].Prime(primeQuerySecondNode);
+
+                    if (async)
+                    {
+                        Assert.ThrowsAsync<OverloadedException>(() => session.ExecuteAsync(new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One)));
+                    }
+                    else
+                    {
+                        Assert.Throws<OverloadedException>(() => session.Execute(new SimpleStatement(cql).SetConsistencyLevel(ConsistencyLevel.One)));
+                    }
+
+                    Assert.AreEqual(11, currentHostRetryPolicy.RequestErrorCounter);
+                    Assert.AreEqual(12, (await nodes[0].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
+                    Assert.AreEqual(0, (await nodes[1].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
+                    Assert.AreEqual(0, (await nodes[2].GetQueriesAsync(cql).ConfigureAwait(false)).Count);
+                }
+            }
+        }
+
+        private class TestExtendedRetryPolicy : IExtendedRetryPolicy
         {
             public long ReadTimeoutCounter;
             public long WriteTimeoutCounter;
@@ -189,10 +269,17 @@ namespace Cassandra.IntegrationTests.Policies.Tests
             }
         }
 
-        class CurrentHostRetryPolicy : IExtendedRetryPolicy
+        private class CurrentHostRetryPolicy : IExtendedRetryPolicy
         {
-            public long RequestErrorConter;
-            public ICollection<int> Retries = new List<int>();
+            private readonly int _maxRetries;
+            private readonly Func<int, Task> _action;
+            public long RequestErrorCounter;
+
+            public CurrentHostRetryPolicy(int maxRetries, Func<int, Task> action)
+            {
+                _maxRetries = maxRetries;
+                _action = action;
+            }
 
             public RetryDecision OnReadTimeout(IStatement query, ConsistencyLevel cl, int requiredResponses, int receivedResponses, bool dataRetrieved, int nbRetry)
             {
@@ -211,8 +298,14 @@ namespace Cassandra.IntegrationTests.Policies.Tests
 
             public RetryDecision OnRequestError(IStatement statement, Configuration config, Exception ex, int nbRetry)
             {
-                Retries.Add(nbRetry);
-                Interlocked.Increment(ref RequestErrorConter);
+                _action?.Invoke(nbRetry).GetAwaiter().GetResult();
+
+                if (nbRetry > _maxRetries)
+                {
+                    return RetryDecision.Rethrow();
+                }
+
+                Interlocked.Increment(ref RequestErrorCounter);
                 return RetryDecision.Retry(null, true);
             }
         }
