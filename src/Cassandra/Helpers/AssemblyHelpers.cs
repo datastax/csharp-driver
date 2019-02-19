71c124f438ac186477aa0b591a9b404d3a30eff8
João Reis <joao.r.reis@outlook.com>
2019-02-19 09:50:15 +0000

    [CSHARP-618] - added DRIVER_NAME and DRIVER_VERSION startup options (#433)
    * added DRIVER_NAME and DRIVER_VERSION startup options

---
+++ src/Cassandra/Helpers/AssemblyHelpers.cs
@@ -0,0 +1,60 @@
+﻿//
+//       Copyright (C) 2019 DataStax Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+using System;
+using System.Reflection;
+
+namespace Cassandra.Helpers
+{
+    internal static class AssemblyHelpers
+    {
+        public static Assembly GetAssembly(Type type)
+        {
+            return type.GetTypeInfo().Assembly;
+        }
+
+        public static Version GetVersionPrefix(Type type)
+        {
+            return AssemblyHelpers.GetVersionPrefix(AssemblyHelpers.GetAssembly(type));
+        }
+
+        public static Version GetVersionPrefix(Assembly assembly)
+        {
+            var assemblyVersion = AssemblyHelpers.GetAssemblyInformationalVersion(assembly);
+            var indexOfVersionSuffix = assemblyVersion.IndexOf('-');
+            var versionPrefix = indexOfVersionSuffix == -1 ? assemblyVersion : assemblyVersion.Substring(0, indexOfVersionSuffix);
+            return Version.Parse(versionPrefix);
+        }
+
+        public static string GetAssemblyInformationalVersion(Assembly assembly)
+        {
+            // InformationalVersion must be used for this driver because GetName().Version returns the assembly version attribute which is wrong
+            // (at the time this comment was written, the assembly version was 3.99.0.0 while the actual driver version is 3.8.0)
+            // and file version doesn't include suffixes like "-alpha"
+            return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+
+        public static string GetAssemblyInformationalVersion(Type type)
+        {
+            return AssemblyHelpers.GetAssemblyInformationalVersion(AssemblyHelpers.GetAssembly(type));
+        }
+
+        public static string GetAssemblyTitle(Type type)
+        {
+            return AssemblyHelpers.GetAssembly(type).GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        }
+    }
+}
---
+++ src/Cassandra/Requests/StartupOptionsFactory.cs
@@ -0,0 +1,69 @@
+﻿// 
+//       Copyright (C) 2019 DataStax Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+
+using System.Collections.Generic;
+using Cassandra.Helpers;
+
+namespace Cassandra.Requests
+{
+    internal class StartupOptionsFactory : IStartupOptionsFactory
+    {
+        public const string CqlVersionOption = "CQL_VERSION";
+        public const string CompressionOption = "COMPRESSION";
+        public const string NoCompactOption = "NO_COMPACT";
+        public const string DriverNameOption = "DRIVER_NAME";
+        public const string DriverVersionOption = "DRIVER_VERSION";
+
+        public const string CqlVersion = "3.0.0";
+        public const string SnappyCompression = "snappy";
+        public const string Lz4Compression = "lz4";
+
+        public IReadOnlyDictionary<string, string> CreateStartupOptions(ProtocolOptions options)
+        {
+            var startupOptions = new Dictionary<string, string>
+            {
+                { StartupOptionsFactory.CqlVersionOption, StartupOptionsFactory.CqlVersion }
+            };
+
+            string compressionName = null;
+            switch (options.Compression)
+            {
+                case CompressionType.LZ4:
+                    compressionName = StartupOptionsFactory.Lz4Compression;
+                    break;
+                case CompressionType.Snappy:
+                    compressionName = StartupOptionsFactory.SnappyCompression;
+                    break;
+            }
+
+            if (compressionName != null)
+            {
+                startupOptions.Add(StartupOptionsFactory.CompressionOption, compressionName);
+            }
+
+            if (options.NoCompact)
+            {
+                startupOptions.Add(StartupOptionsFactory.NoCompactOption, "true");
+            }
+
+            startupOptions.Add(StartupOptionsFactory.DriverNameOption, AssemblyHelpers.GetAssemblyTitle(typeof(StartupOptionsFactory)));
+            startupOptions.Add(
+                StartupOptionsFactory.DriverVersionOption, AssemblyHelpers.GetAssemblyInformationalVersion(typeof(StartupOptionsFactory)));
+
+            return startupOptions;
+        }
+    }
+}
