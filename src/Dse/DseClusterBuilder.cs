579038ce072736fcad51c640b6fb2e01af58e071
João Reis <joao.r.reis@outlook.com>
2019-05-21 14:17:56 +0100

    add missing method to DseClusterBuilder

--- src/Dse/DseClusterBuilder.cs
+++ src/Dse/DseClusterBuilder.cs
@@ -638,6 +638,51 @@ namespace Dse
         }
 
         /// <summary>
+        /// <para>
+        /// If not set through this method, the default value options will be used (metadata synchronization is enabled by default). The api reference of <see cref="MetadataSyncOptions"/>
+        /// specifies what is the default for each option.
+        /// </para>
+        /// <para>
+        /// In case you disable Metadata synchronization, please ensure you invoke <see cref="ICluster.RefreshSchemaAsync"/> in order to keep the token metadata up to date
+        /// otherwise you will not be getting everything you can out of token aware routing, i.e. <see cref="TokenAwarePolicy"/>, which is used by <see cref="DseLoadBalancingPolicy"/> and is the default. 
+        /// </para>
+        /// <para>
+        /// Disabling this feature has the following impact:
+        /// 
+        /// <list type="bullet">
+        /// 
+        /// <item><description>
+        /// Token metadata will not be computed and stored.
+        /// This means that token aware routing (<see cref="TokenAwarePolicy"/>, which is used by <see cref="DseLoadBalancingPolicy"/> and is the default) will only work correctly
+        /// if you keep the token metadata up to date using the <see cref="ICluster.RefreshSchemaAsync"/> method.
+        /// If you wish to go this route of manually refreshing the metadata then
+        /// it's recommended to refresh only the keyspaces that this application will use, by passing the <code>keyspace</code> parameter.
+        /// </description></item>
+        /// 
+        /// <item><description>
+        /// Keyspace metadata will not be cached by the driver. Every time you call methods like <see cref="Metadata.GetTable"/>, <see cref="Metadata.GetKeyspace"/>
+        /// and other similar methods of the <see cref="Metadata"/> class, the driver will query that data on demand and will not cache it.
+        /// </description></item>
+        /// 
+        /// <item><description>
+        /// The driver will not handle <code>SCHEMA_CHANGED</code> responses. This means that when you execute schema changing statements through the driver, it will
+        /// not update the schema or topology metadata automatically before returning.
+        /// </description></item>
+        /// 
+        /// </list>
+        /// </para>
+        /// </summary>
+        /// <summary>
+        /// </summary>
+        /// <param name="metadataSyncOptions">The new options to set.</param>
+        /// <returns>This Builder.</returns>
+        public new DseClusterBuilder WithMetadataSyncOptions(MetadataSyncOptions metadataSyncOptions)
+        {
+            base.WithMetadataSyncOptions(metadataSyncOptions);
+            return this;
+        }
+
+        /// <summary>
         /// Builds the cluster with the configured set of initial contact points and policies.
         /// </summary>
         /// <returns>
