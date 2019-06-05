bb216d086d5d8c91a7078a920137a056b4029c64
João Reis <joao.r.reis@outlook.com>
2019-06-05 17:19:46 +0100

    add timestamp test

--- src/Cassandra.IntegrationTests/Core/TimestampTests.cs
+++ src/Cassandra.IntegrationTests/Core/TimestampTests.cs
@@ -59,6 +59,49 @@ namespace Cassandra.IntegrationTests.Core
             }
         }
 
+        [Test]
+        public void Should_UseProvidedTimestamp_When_TimestampIsNotAValidDateTimeOffset()
+        {
+            var generator = new TicksTimestampGenerator();
+            var unixEpochStart = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+            using (var cluster = Cluster.Builder()
+                                        .WithTimestampGenerator(generator)
+                                        .AddContactPoint(TestCluster.InitialContactPoint)
+                                        .Build())
+            {
+                var session = cluster.Connect();
+                var ks = TestUtils.GetUniqueKeyspaceName().ToLowerInvariant();
+                var table = TestUtils.GetUniqueTableName().ToLowerInvariant();
+                session.CreateKeyspaceIfNotExists(ks);
+                session.ChangeKeyspace(ks);
+                session.Execute($"CREATE TABLE IF NOT EXISTS {table} (id int primary key, random_value text)");
+                var insertPrepare = session.Prepare($"INSERT INTO {table} (id, random_value) VALUES (?, ?)");
+
+                var dt = DateTimeOffset.UtcNow;
+                var stmt1 = insertPrepare.Bind(1, "123");
+                var stmt2 = insertPrepare.Bind(2, "321").SetTimestamp(dt);
+
+                if (GetProtocolVersion() < ProtocolVersion.V3)
+                {
+                    Assert.Throws<NotSupportedException>(() => session.Execute(stmt1));
+                    Assert.Throws<NotSupportedException>(() => session.Execute(stmt2));
+                }
+                else
+                {
+                    session.Execute(stmt1);
+                    var ticksStmt1 = generator.GetTicks();
+                    session.Execute(stmt2);
+
+                    var rs1 = session.Execute($"SELECT WRITETIME(random_value) FROM {table} WHERE id = {1}");
+                    var rs2 = session.Execute($"SELECT WRITETIME(random_value) FROM {table} WHERE id = {2}");
+
+                    Assert.AreEqual(6, generator.GetCounter()); // 2x selects, 1x insert (the other has a statement timestamp), create keyspace, use keyspace, create table
+                    Assert.AreEqual(ticksStmt1, rs1.Single().GetValue<long>(0));
+                    Assert.AreEqual((dt - unixEpochStart).Ticks / 10, rs2.Single().GetValue<long>(0));
+                }
+            }
+        }
+
         private class TestTimestampGenerator : ITimestampGenerator
         {
             private int _counter;
@@ -71,6 +114,30 @@ namespace Cassandra.IntegrationTests.Core
             public long Next()
             {
                 return Interlocked.Increment(ref _counter);
+            }
+        }
+
+        private class TicksTimestampGenerator : ITimestampGenerator
+        {
+            private int _counter;
+            private long _ticks;
+
+            public long GetCounter()
+            {
+                return Volatile.Read(ref _counter);
+            }
+
+            public long GetTicks()
+            {
+                return Volatile.Read(ref _ticks);
+            }
+
+            public long Next()
+            {
+                var ticks = DateTime.UtcNow.Ticks;
+                Interlocked.Increment(ref _counter);
+                Interlocked.Exchange(ref _ticks, ticks);
+                return ticks;
             }
         }
     }
