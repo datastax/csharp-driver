ba2a02cf95310d1decd7a6bd172c260a6a3b02a7
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2019-08-09 14:34:55 +0500

    Add implementations of metrics abstractions using AppMetrics dependency

---
+++ src/Cassandra/Metrics/AppMetricsImpl/AppMetricsDriverHistogram.cs
@@ -0,0 +1,39 @@
+// 
+//       Copyright (C) 2019 DataStax Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+
+#if NETSTANDARD2_0
+
+using App.Metrics.Histogram;
+using Cassandra.Metrics.DriverAbstractions;
+
+namespace Cassandra.Metrics.AppMetricsImpl
+{
+    internal class AppMetricsDriverHistogram : IDriverHistogram
+    {
+        private readonly IHistogram _histogram;
+
+        public AppMetricsDriverHistogram(IHistogram histogram)
+        {
+            _histogram = histogram;
+        }
+
+        public void Update(long value)
+        {
+            _histogram.Update(value);
+        }
+    }
+}
+#endif
---
+++ src/Cassandra/Metrics/AppMetricsImpl/AppMetricsDriverTimeHandler.cs
@@ -0,0 +1,39 @@
+// 
+//       Copyright (C) 2019 DataStax Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+
+#if NETSTANDARD2_0
+
+using App.Metrics.Timer;
+using Cassandra.Metrics.DriverAbstractions;
+
+namespace Cassandra.Metrics.AppMetricsImpl
+{
+    internal class AppMetricsDriverTimeHandler : IDriverTimeHandler
+    {
+        private TimerContext _timerContext;
+
+        public AppMetricsDriverTimeHandler(TimerContext timerContext)
+        {
+            _timerContext = timerContext;
+        }
+
+        public void EndRecording()
+        {
+            _timerContext.Dispose();
+        }
+    }
+}
+#endif
