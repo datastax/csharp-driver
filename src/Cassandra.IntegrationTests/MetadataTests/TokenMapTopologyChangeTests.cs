048c098f2f3e6557685b0d75ea8217208b4ec202
João Reis <joao.r.reis@outlook.com>
2019-12-20 03:40:50 +0000

    fix tests

--- src/Cassandra.IntegrationTests/MetadataTests/TokenMapTopologyChangeTests.cs
+++ src/Cassandra.IntegrationTests/MetadataTests/TokenMapTopologyChangeTests.cs
@@ -71,6 +71,8 @@ namespace Cassandra.IntegrationTests.MetadataTests
 
             this.TestCluster.DecommissionNode(1);
 
+            this.TestCluster.Remove(1);
+
             TestHelper.RetryAssert(() =>
             {
                 Assert.AreEqual(2, ClusterObjSync.Metadata.Hosts.Count);
