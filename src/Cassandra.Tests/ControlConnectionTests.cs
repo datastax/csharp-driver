58b1f4a7b20a1e0492757b614d096442ca0dba48
João Reis <joao.r.reis@outlook.com>
2020-03-17 13:07:26 +0000

    fix bug where controlconnection attempts to connect to down or ignored hosts

--- src/Cassandra.Tests/ControlConnectionTests.cs
+++ src/Cassandra.Tests/ControlConnectionTests.cs
@@ -16,7 +16,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using Cassandra.Connections;
 using Cassandra.ExecutionProfiles;
 using Cassandra.ProtocolEvents;
@@ -44,10 +46,10 @@ namespace Cassandra.Tests
                 TimeSpan.FromMilliseconds(config.MetadataSyncOptions.MaxTotalRefreshSchemaDelay));
         }
 
-        private ControlConnection NewInstance(Configuration config, Metadata metadata)
+        private ControlConnection NewInstance(IInternalCluster cluster, Configuration config, Metadata metadata)
         {
             return new ControlConnection(
-                Mock.Of<IInternalCluster>(), 
+                cluster, 
                 GetEventDebouncer(config), 
                 ProtocolVersion.MaxSupported, 
                 config, metadata, 
@@ -60,16 +62,12 @@ namespace Cassandra.Tests
                 });
         }
 
-        private ControlConnection NewInstance(Metadata metadata)
-        {
-            return NewInstance(new Configuration(), metadata);
-        }
-
         [Test]
         public void UpdateLocalNodeInfoModifiesHost()
         {
-            var metadata = new Metadata(new Configuration());
-            var cc = NewInstance(metadata);
+            var config = new Configuration();
+            var metadata = new Metadata(config);
+            var cc = NewInstance(Mock.Of<IInternalCluster>(), config, metadata);
             cc.Host = TestHelper.CreateHost("127.0.0.1");
             var row = TestHelper.CreateRow(new Dictionary<string, object>
             {
@@ -86,8 +84,9 @@ namespace Cassandra.Tests
         [Test]
         public void UpdatePeersInfoModifiesPool()
         {
-            var metadata = new Metadata(new Configuration());
-            var cc = NewInstance(metadata);
+            var config = new Configuration();
+            var metadata = new Metadata(config);
+            var cc = NewInstance(Mock.Of<IInternalCluster>(), config, metadata);
             cc.Host = TestHelper.CreateHost("127.0.0.1");
             metadata.AddHost(cc.Host.Address);
             var hostAddress2 = IPAddress.Parse("127.0.0.2");
@@ -115,8 +114,9 @@ namespace Cassandra.Tests
         [Test]
         public void UpdatePeersInfoWithNullRpcIgnores()
         {
-            var metadata = new Metadata(new Configuration());
-            var cc = NewInstance(metadata);
+            var config = new Configuration();
+            var metadata = new Metadata(config);
+            var cc = NewInstance(Mock.Of<IInternalCluster>(), config, metadata);
             cc.Host = TestHelper.CreateHost("127.0.0.1");
             metadata.AddHost(cc.Host.Address);
             var rows = TestHelper.CreateRows(new List<Dictionary<string, object>>
@@ -146,7 +146,7 @@ namespace Cassandra.Tests
                     AddressTranslator = translatorMock.Object,
                     StartupOptionsFactory = Mock.Of<IStartupOptionsFactory>()
                 }.Build();
-            var cc = NewInstance(config, metadata);
+            var cc = NewInstance(Mock.Of<IInternalCluster>(), config, metadata);
             cc.Host = TestHelper.CreateHost("127.0.0.1");
             metadata.AddHost(cc.Host.Address);
             var hostAddress2 = IPAddress.Parse("127.0.0.2");
@@ -163,6 +163,55 @@ namespace Cassandra.Tests
             Assert.AreEqual(portNumber, invokedEndPoints[0].Port);
             Assert.AreEqual(hostAddress3, invokedEndPoints[1].Address);
             Assert.AreEqual(portNumber, invokedEndPoints[1].Port);
+        }
+        
+        [Test]
+        public void ShouldNotAttemptDownOrIgnoredHosts()
+        {
+            var config = new TestConfigurationBuilder()
+            {
+                SocketOptions = new SocketOptions().SetConnectTimeoutMillis(100).SetReadTimeoutMillis(100),
+                Policies = new Cassandra.Policies(
+                    new ClusterUnitTests.FakeHostDistanceLbp(new Dictionary<string, HostDistance>
+                    {
+                        {"127.0.0.1", HostDistance.Local},
+                        {"127.0.0.2", HostDistance.Local},
+                        {"127.0.0.3", HostDistance.Ignored},
+                        {"127.0.0.4", HostDistance.Local}
+                    }),
+                    new ConstantReconnectionPolicy(1000),
+                    new DefaultRetryPolicy())
+            }.Build();
+            var cluster = Mock.Of<IInternalCluster>();
+            var metadata = new Metadata(config);
+            var cc = NewInstance(cluster, config, metadata);
+            cc.Host = TestHelper.CreateHost("127.0.0.1");
+            metadata.AddHost(cc.Host.Address);
+            var hostAddress2 = IPAddress.Parse("127.0.0.2");
+            var hostAddress3 = IPAddress.Parse("127.0.0.3");
+            var hostAddress4 = IPAddress.Parse("127.0.0.4");
+            var rows = TestHelper.CreateRows(new List<Dictionary<string, object>>
+            {
+                new Dictionary<string, object>{{"rpc_address", hostAddress2}, {"peer", null}, { "data_center", "ut-dc2" }, { "rack", "ut-rack2" }, {"tokens", null}, {"release_version", "2.1.5"}},
+                new Dictionary<string, object>{{"rpc_address", IPAddress.Parse("0.0.0.0")}, {"peer", hostAddress3}, { "data_center", "ut-dc3" }, { "rack", "ut-rack3" }, {"tokens", null}, {"release_version", "2.1.5"}},
+                new Dictionary<string, object>{{"rpc_address", IPAddress.Parse("0.0.0.0")}, {"peer", hostAddress4}, { "data_center", "ut-dc3" }, { "rack", "ut-rack2" }, {"tokens", null}, {"release_version", "2.1.5"}}
+            });
+            cc.UpdatePeersInfo(rows, cc.Host);
+            Assert.AreEqual(4, metadata.AllHosts().Count);
+            var host2 = metadata.GetHost(new IPEndPoint(hostAddress2, ProtocolOptions.DefaultPort));
+            Assert.NotNull(host2);
+            host2.SetDown();
+            var host3 = metadata.GetHost(new IPEndPoint(hostAddress3, ProtocolOptions.DefaultPort));
+            Assert.NotNull(host3);
+            
+            Mock.Get(cluster)
+                .Setup(c => c.RetrieveAndSetDistance(It.IsAny<Host>()))
+                .Returns<Host>(h => config.Policies.LoadBalancingPolicy.Distance(h));
+            Mock.Get(cluster).Setup(c => c.AllHosts()).Returns(() => metadata.AllHosts());
+            config.Policies.LoadBalancingPolicy.Initialize(cluster);
+
+            var ex = Assert.ThrowsAsync<NoHostAvailableException>(() => cc.Reconnect());
+            CollectionAssert.AreEquivalent(new [] { "127.0.0.1", "127.0.0.4" }, ex.Errors.Keys.Select(e => e.Address.ToString()));
         }
     }
 }
