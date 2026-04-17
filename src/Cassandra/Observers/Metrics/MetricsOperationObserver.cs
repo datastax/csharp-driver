98caaa12f12d670212293d9c566af6bd7c9606b2
SAY-5 <SAY-5@users.noreply.github.com>
2026-04-16 21:49:44 -0700

    Fix occured typo in MetricsOperationObserver.cs
    Signed-off-by: SAY-5 <SAY-5@users.noreply.github.com>

--- src/Cassandra/Observers/Metrics/MetricsOperationObserver.cs
+++ src/Cassandra/Observers/Metrics/MetricsOperationObserver.cs
@@ -83,7 +83,7 @@ namespace Cassandra.Observers.Metrics
 
         private static void LogError(Exception ex)
         {
-            Logger.Warning("An error occured while recording metrics for a connection operation. Exception = {0}", ex.ToString());
+            Logger.Warning("An error occurred while recording metrics for a connection operation. Exception = {0}", ex.ToString());
         }
     }
 }
